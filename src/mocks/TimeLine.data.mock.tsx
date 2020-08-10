89a81fc524ddff239992c8023a6f7c3e0b97154d
Tejas Beri <tejas.beri@transport.nsw.gov.au>
2020-08-10 17:21:20 +1000

    add box shadow

--- src/mocks/TimeLine.data.mock.tsx
+++ src/mocks/TimeLine.data.mock.tsx
@@ -25,5 +25,20 @@ export const TimelineData: TimelineItem[] = [
     title: "Blogs",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
+  },
+  {
+    title: "Hustle",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
+  },
+  {
+    title: "Code jam",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
+  },
+  {
+    title: "Have fun",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s"
   }
 ];
