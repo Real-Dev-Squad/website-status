1bee0b2c2b7640cf1813b059866c083ff3663fda
Khanjan <khanjanvarma9@gmail.com>
2020-12-20 16:45:14 +0530

    Specific type description for participants and content done

--- src/components/challenges/complete.tsx
+++ src/components/challenges/complete.tsx
@@ -4,13 +4,26 @@ import Details from './details';
 import Participants from './participants';
 
 type CompleteProps = {
-  content: {
-    is_user_subscribed: number;
+  content: {id: number;
     title: string;
     level: string;
     start_date: string;
     end_date: string;
-    participants: [];
+    participants: {user_id: string,
+      first_name: string,
+      last_name: string,
+      yoe: number,
+      company: string,
+      designation: string,
+      img: string,
+      github_id: string,
+      linkedin_id: string,
+      twitter_id: string,
+      instagram_id: string,
+      is_member: number,
+      rds_member_id: string}[];
+    is_active: number;
+    is_user_subscribed: number;
   };
 };
 
