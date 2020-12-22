de2b0d8e2d0fd6dd1dab8827b9beb634386da21f
Khanjan <khanjanvarma9@gmail.com>
2020-12-22 23:43:00 +0530

    Title added on challenge and mine page

--- src/pages/mine.tsx
+++ src/pages/mine.tsx
@@ -2,10 +2,14 @@ import { FunctionComponent } from 'react';
 import Layout from '../components/Layout';
 import Navbar from '../components/navbar';
 import Title from '../components/Title';
+import { Helmet } from 'react-helmet';
 
 const Mine: FunctionComponent = () => {
   return (
     <Layout>
+      <Helmet>
+        <title>Mine</title>
+      </Helmet>
       <Navbar page="Mine" />
       <Title>This is about page</Title>
     </Layout>
