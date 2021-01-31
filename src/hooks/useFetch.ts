e1607c037ac7a1be589870d35b875b4456ed740f
Nikhil <nikhilbhandarkar1998@gmail.com>
2021-01-31 18:13:18 +0530

    UseFetch generic

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import fetch from '@/helperFunctions/fetch';
 
-const useFetch = (url: string) => {
+const useFetch = (url: string, options: object = {}) => {
   const [response, setResponse] = useState<any>({});
   const [error, setError] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -13,11 +13,10 @@ const useFetch = (url: string) => {
         const res = await fetch({
           url,
           method: 'get',
+          ...options,
         });
         setResponse(res.data);
-        setIsLoading(false);
       } catch (err) {
-        setIsLoading(false);
         setError(err);
       } finally {
         setIsLoading(false);
