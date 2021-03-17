aa1f377e4d56060a4b4290096602555cd1061c87
Aman <saxenaaman628@gmail.com>
2021-03-17 21:09:30 +0530

    Adding mine

---
+++ src/components/tasks/mine_card/index.tsx
@@ -0,0 +1,34 @@
+import { FC } from 'react';
+import classNames from '@/components/tasks/mine_card/card.module.scss';
+import { task } from '@/components/constants/types';
+
+type Props = {
+  content: task,
+};
+
+const informationElement = (title: string, value: string) => (
+  <span className={classNames.statusElement}>
+    <span className={classNames.statusLable}>{`${title}: `}</span>
+    <strong>{value}</strong>
+  </span>
+);
+
+const Card: FC<Props> = ({ content }) => {
+  const {
+    title,
+    startedOn,
+    status,
+  } = content;
+
+  return (
+    <div className={classNames.card}>
+      <span className={classNames.prTitle}>{title}</span>
+      {informationElement('Started', startedOn)}
+      <div className={classNames.cardFooter}>
+        {informationElement('Status', status)}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/pages/mine.tsx
+++ src/pages/mine.tsx
@@ -1,11 +1,71 @@
-import { FC } from 'react';
-import Head from '@/components/head';
-import Layout from '@/components/Layout';
+import { FC, useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
 
-const Mine: FC = () => (
-  <Layout>
-    <Head title="Mine" />
-  </Layout>
-);
+
+import Layout from '@/components/Layout';
+import Card from '@/components/tasks/mine_card';
+import useFetch from '@/hooks/useFetch';
+import classNames from '@/styles/tasks.module.scss';
+import { task } from '@/components/constants/types';
+// const Mine: FC = () => (
+//   <Layout>
+//     <Head title="Mine" />
+//   </Layout>
+// );
+
+// export default Mine;
+const TASKS_URL = `https://run.mocky.io/v3/f8799d5e-03b4-4a96-b684-65557b97ecd9`;
+
+const CardList = (tasks: task[]) => tasks.map(
+  (item: task) => <Card content={item} key={item.id} />,
+
+
+
+const Mine: FC = () => {
+  const [tasks, setTasks] = useState<task[]>([]);
+  
+
+  const {
+    response,
+    error,
+    isLoading,
+  } = useFetch(TASKS_URL);
+
+  useEffect(() => {
+    if ('tasks' in response) {
+      setTasks(response.tasks);
+
+    }
+  }, [isLoading, response]);
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Tasks | Status Real Dev Squad</title>
+      </Helmet>
+
+      <div className="container">
+        {
+          (!!error) && (
+            <p>Something went wrong, please contact admin!</p>
+          )
+        }
+        {
+          (isLoading)
+            ? (
+              <p>Loading...</p>
+            )
+            : (
+              <div className={classNames.container}>
+               {CardList(tasks)} 
+
+              </div>
+            )
+        }
+      </div>
+    </Layout>
+  );
+};
 
 export default Mine;
+
