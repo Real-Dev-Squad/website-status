af436599aef515e78de107e1c34491df56c0d3fd
Aman Anand <aman1998anand@gmail.com>
2021-03-27 16:38:22 +0530

    fixed build errors

---
+++ src/pages/completed.tsx
@@ -0,0 +1,66 @@
+import type { FC } from 'react';
+import { useState, useEffect } from 'react';
+import Head from '@/components/head';
+import Layout from '@/components/Layout';
+import Card from '@/components/tasks/card';
+import useFetch from '@/hooks/useFetch';
+import classNames from '@/styles/tasks.module.scss';
+import { task } from '@/components/constants/types';
+import Accordion from '@/components/Accordion';
+
+const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks`;
+
+const renderCardList = (tasks: task[]) => tasks.map(
+  (item: task) => <Card content={item} key={item.id} />,
+);
+
+const Completed: FC = () => {
+  const [tasks, setTasks] = useState<task[]>([]);
+  const [completeTasks, setCompleteTasks] = useState<any>(null);
+
+  const {
+    response,
+    error,
+    isLoading,
+  } = useFetch(TASKS_URL);
+
+  useEffect(() => {
+    if ('tasks' in response) {
+      setTasks(response.tasks);
+
+      const complete = tasks.filter(
+        (item: task) => item.status === 'Completed',
+      );
+      setCompleteTasks(complete);
+    }
+  }, [isLoading, response]);
+
+  return (
+    <Layout>
+      <Head title="Tasks" />
+
+      <div className="container">
+        {
+            (!!error) && (
+            <p>Something went wrong, please contact admin!</p>
+            )
+        }
+        {
+            (isLoading)
+              ? (
+                <p>Loading...</p>
+              )
+              : (
+                <div className={classNames.container}>
+                  <Accordion title="Completed">
+                    {renderCardList(completeTasks)}
+                  </Accordion>
+                </div>
+              )
+        }
+      </div>
+    </Layout>
+  );
+};
+
+export default Completed;
