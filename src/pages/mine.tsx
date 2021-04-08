6418e0656f66d0da3840cafbdcdcd0d297d4ea27
Aman <saxenaaman628@gmail.com>
2021-04-08 10:35:18 +0530

    Remove Mine_card

--- src/pages/mine.tsx
+++ src/pages/mine.tsx
@@ -6,7 +6,7 @@ import useFetch from '@/hooks/useFetch';
 import classNames from '@/styles/tasks.module.scss';
 import { task } from '@/components/constants/types';
 
-const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks`;
+const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/task0/self`;
 
 function CardList(tasks: task[]) {
   return tasks.map(
