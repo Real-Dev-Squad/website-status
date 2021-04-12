ba73414590b6364b8f8f3e2439b8a90ece499f81
sarojpunde <saarojpunde@gmail.com>
2021-04-12 23:58:09 +0530

    Issue-86:- Show readable date format in tasks

--- src/components/tasks/card/index.tsx
+++ src/components/tasks/card/index.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 import classNames from '@/components/tasks/card/card.module.scss';
 import { task } from '@/components/constants/types';
 
+const moment = require('moment');
+
 type Props = {
   content: task,
 };
@@ -24,11 +26,33 @@ const Card: FC<Props> = ({ content }) => {
 
   const ownerProfilePic = `${process.env.NEXT_PUBLIC_GITHUB_IMAGE_URL}${ownerId}/img.png`;
 
+  const localStartedOn = new Date(startedOn);
+  const fromNowStartedOn = moment(localStartedOn).fromNow();
+  const readableStartedOn = `${localStartedOn.toLocaleDateString()}, ${localStartedOn.toLocaleTimeString()}`;
+
+  const localEndsOn = new Date(endsOn);
+  const fromNowEndsOn = moment(localEndsOn).fromNow();
+  const readableEndsOn = `${localEndsOn.toLocaleDateString()}, ${localEndsOn.toLocaleTimeString()}`;
+
   return (
     <div className={classNames.card}>
       <span className={classNames.prTitle}>{title}</span>
-      {informationElement('Estimated completion', endsOn)}
-      {informationElement('Started', startedOn)}
+      <div className={classNames.datetime}>
+        <span className={classNames.nothover}>
+          {informationElement('Estimated completion', fromNowEndsOn)}
+        </span>
+        <span className={classNames.onhover}>
+          {informationElement('Estimated completion', readableEndsOn)}
+        </span>
+      </div>
+      <div className={classNames.datetime}>
+        <span className={classNames.nothover}>
+          {informationElement('Started', fromNowStartedOn)}
+        </span>
+        <span className={classNames.onhover}>
+          {informationElement('Started', readableStartedOn)}
+        </span>
+      </div>
       <div className={classNames.cardFooter}>
         <div className={classNames.profilePicture}>
           <img
