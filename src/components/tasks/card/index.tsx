ce1bb2ad4ded4c50d6b4a66ff66131abaef8f335
pavangbhat <pavanbhat17@gmail.com>
2021-04-22 23:18:34 +0530

    fixed font and added few things

--- src/components/tasks/card/index.tsx
+++ src/components/tasks/card/index.tsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-constant-condition */
 import { FC, useState } from 'react';
 import classNames from '@/components/tasks/card/card.module.scss';
 import { task } from '@/components/constants/types';
+import Image from 'next/image';
 
 const moment = require('moment');
 
@@ -26,19 +28,30 @@ const Card: FC<Props> = ({ content }) => {
   const localEndsOn = new Date(parseInt(endsOn, 10) * 1000);
   const fromNowEndsOn = moment(localEndsOn).fromNow();
 
+  const statusFontColor = status === 'active' || 'assigned' || 'completed' || 'pending' ? '#00a337' : '#f83535';
+
   return (
     <div className={classNames.card}>
       <div className={classNames.cardItems}>
         <span className={classNames.cardTitle}>{title}</span>
         <span>
           <span className={classNames.cardSpecialFont}>Status:</span>
-          <span className={classNames.cardStatusFont}>{status}</span>
+          <span className={classNames.cardStatusFont} style={{ color: statusFontColor }}>
+            {status}
+          </span>
         </span>
       </div>
       <div className={classNames.cardItems}>
         <span>
-          <img className={classNames.calendarIcon} alt="" />
-          <span className={classNames.cardSpecialFont}>Due Date</span>
+          <Image
+            src="/calendar-icon.png"
+            alt="calendar icon"
+            width="25px"
+            height="25px"
+          />
+          <span className={classNames.cardSpecialFont}>
+            Due Date
+          </span>
           <span className={classNames.cardStrongFont}>{fromNowEndsOn}</span>
         </span>
       </div>
