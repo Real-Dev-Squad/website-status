0ccc5ec709770f73cdfead85ed7ec947f6bc1704
Ankita Bannore <bannoreankita20@gmail.com>
2021-04-24 16:21:31 +0530

    Added section code in pages/challenges

--- src/components/constants/types.ts
+++ src/components/constants/types.ts
@@ -21,3 +21,31 @@ export type task = {
   lossRate: award,
   isNoteworthy: boolean
 }
+
+export type IndexProps = {
+  content: {
+    id: number;
+    title: string;
+    level: string;
+    start_date: string;
+    end_date: string;
+    participants: {
+      user_id: string;
+      first_name: string;
+      last_name: string;
+      yoe: number;
+      company: string;
+      designation: string;
+      img: string;
+      github_id: string;
+      linkedin_id: string;
+      twitter_id: string;
+      instagram_id: string;
+      is_member: number;
+      rds_member_id: string;
+    }[];
+    is_active: number;
+    is_user_subscribed: number;
+  }[];
+  screen: string;
+}
--- src/pages/challenges.tsx
+++ src/pages/challenges.tsx
@@ -1,17 +1,87 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import Head from '@/components/head';
 import Layout from '@/components/Layout';
-import Section from '@/components/section';
+import Active from '@/components/challenges/active';
+import Complete from '@/components/challenges/complete';
+import Accordion from '@/components/Accordion';
 import { CHALLENGES_SCREEN } from '@/components/constants/display-sections.js';
-import ChallengesJson from '@/mocks/challenges.json';
+import useFetch from '@/hooks/useFetch';
+import { IndexProps } from '@/components/constants/types';
+import classNames from '@/styles/tasks.module.scss';
 
-const Challenges: FC = () => (
-  <Layout>
-    <Head title="Challenges" />
-    <div>
-      <Section content={ChallengesJson} screen={CHALLENGES_SCREEN} />
-    </div>
-  </Layout>
-);
+const CHALLENGES_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/challenges`;
+
+const Challenges: FC = () => {
+  let challenges:any[] = [];
+  const [filterChallenge, setFilterChallenge] = useState<any>([]);
+
+  const { response, error, isLoading } = useFetch(CHALLENGES_URL);
+
+  useEffect(() => {
+    if ('challenges' in response) {
+      challenges = response.challenges;
+      setFilterChallenge(challenges);
+    }
+  }, [isLoading, response]);
+
+  const section = (sectionContent:IndexProps['content'], screen:IndexProps['screen']) => {
+    let activeContent;
+    let completeContent;
+
+    if (screen === CHALLENGES_SCREEN) {
+      activeContent = sectionContent
+        .filter((challenge) => challenge.is_active)
+        .map((activeChallenge) => (
+          <Active key={activeChallenge.id} content={activeChallenge} />
+        ));
+      completeContent = sectionContent
+        .filter((challenge) => !challenge.is_active)
+        .map((completedChallenge) => (
+          <Complete key={completedChallenge.id} content={completedChallenge} />
+        ));
+    }
+    return (
+      <section className={classNames.section}>
+        <div className={classNames.active}>
+          <Accordion open title="Active">
+            {activeContent}
+          </Accordion>
+        </div>
+        <div className={classNames.complete}>
+          <Accordion open title="Completed">
+            {completeContent}
+          </Accordion>
+        </div>
+      </section>
+    );
+  };
+
+  return (
+    <Layout>
+      <Head title="Challenges" />
+      <div className={classNames.container}>
+        {!!error && <p>Something went wrong, please contact admin!</p>}
+        {
+          isLoading
+            ? (
+              <p>Loading...</p>
+            ) : (
+              <>
+                {
+                 filterChallenge.length > 0
+                   ? (
+                     <div>
+                       {section(filterChallenge, CHALLENGES_SCREEN)}
+                     </div>
+                   )
+                   : (!error && 'No Challenges Found')
+                }
+              </>
+            )
+        }
+      </div>
+    </Layout>
+  );
+};
 
 export default Challenges;
