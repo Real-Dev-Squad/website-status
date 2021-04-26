b3e2b6a4e7aa9967e63642888336b0bc0aae0f50
Ankita Bannore <bannoreankita20@gmail.com>
2021-04-26 23:43:58 +0530

    Filtered the active & completed challenge in pages/challenges

--- src/pages/challenges.tsx
+++ src/pages/challenges.tsx
@@ -10,15 +10,15 @@ import classNames from '@/styles/tasks.module.scss';
 
 const CHALLENGES_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/challenges`;
 
-function renderCardList(challengeSection: challenge['content'], key:string) {
+const renderCardList = (challengeSection: challenge['content'], key:string) => {
   if (key === 'Active') {
     return challengeSection.map((item) => <Active content={item} key={item.id} />);
   }
   return challengeSection.map((item) => <Complete content={item} key={item.id} />);
-}
+};
 
 const Challenges: FC = () => {
-  let challenges: challenge['content'] = [];
+  let challenges: challenge['content'][] = [];
 
   const [filteredChallenge, setFilteredChallenge] = useState<any>([]);
 
@@ -28,21 +28,10 @@ const Challenges: FC = () => {
     if ('challenges' in response) {
       challenges = response.challenges;
       const challengeMap: any = [];
-      challenges.forEach((item) => {
-        if (item.is_active) {
-          if ('Active' in challengeMap) {
-            challengeMap.Active = [...challengeMap.Active, item];
-          } else {
-            challengeMap.Active = [item];
-          }
-        } else if (!item.is_active) {
-          if ('Completed' in challengeMap) {
-            challengeMap.Completed = [...challengeMap.Completed, item];
-          } else {
-            challengeMap.Completed = [item];
-          }
-        }
-      });
+
+      challengeMap.Active = challenges.filter((task) => task.is_active);
+      challengeMap.Complete = challenges.filter((task) => !task.is_active);
+
       setFilteredChallenge(challengeMap);
     }
   }, [isLoading, response]);
@@ -62,9 +51,13 @@ const Challenges: FC = () => {
               {
                 Object.keys(filteredChallenge).length > 0
                   ? Object.keys(filteredChallenge).map((key) => (
+                    filteredChallenge[key].length > 0
+                    && (
                     <Accordion open title={key} key={key}>
                       {renderCardList(filteredChallenge[key], key)}
                     </Accordion>
+                    )
+
                   )) : (!error && 'No Challenges Found')
               }
             </>
