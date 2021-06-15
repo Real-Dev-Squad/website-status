1a812f24e2e4ce174147c92b5f71973e38ffd0fd
Vividh Bhardwaj <bhardwajvividh@gmail.com>
2021-06-15 15:37:42 +0530

    Make the shimmer effect look like the card

--- src/components/Loaders/cardShimmer/index.js
+++ src/components/Loaders/cardShimmer/index.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import classNames from './cardShimmer.module.scss';
+import React from "react";
+import classNames from "./cardShimmer.module.scss";
 
 const CardShimmer = () => (
-
   <div className={`${classNames.card} ${classNames.br}`}>
     <div className={classNames.wrapper}>
-      <div className={`${classNames.title} ${classNames.br} ${classNames.animate}`} />
-      <div className={`${classNames.create} ${classNames.br} ${classNames.animate}`} />
-      <div className={`${classNames.comment} ${classNames.br} ${classNames.animate}`} />
-      <div className={`${classNames.comment} ${classNames.br} ${classNames.animate}`} />
-      <div className={`${classNames.button} ${classNames.animate}`} />
+      <div
+        className={`${classNames.title} ${classNames.br} ${classNames.animate}`}
+      />
+      <div
+        className={`${classNames.create} ${classNames.br} ${classNames.animate}`}
+      />
+      <div
+        className={`${classNames.comment} ${classNames.br} ${classNames.animate}`}
+      />
+      <div
+        className={`${classNames.comment} ${classNames.br} ${classNames.animate}`}
+      />
+      {/* <div className={`${classNames.button} ${classNames.animate}`} /> */}
     </div>
   </div>
 );
