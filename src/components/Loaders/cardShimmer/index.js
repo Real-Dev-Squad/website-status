af6f4be0dd9577719ce77a829a41e5792864443f
Vividh Bhardwaj <bhardwajvividh@gmail.com>
2021-07-14 22:45:03 +0530

    Nested the css code in sass style

--- src/components/Loaders/cardShimmer/index.js
+++ src/components/Loaders/cardShimmer/index.js
@@ -4,18 +4,10 @@ import classNames from './cardShimmer.module.scss';
 const CardShimmer = () => (
   <div className={`${classNames.card} ${classNames.br}`}>
     <div className={classNames.wrapper}>
-      <div
-        className={`${classNames.title} ${classNames.br} ${classNames.animate}`}
-      />
-      <div
-        className={`${classNames.create} ${classNames.br} ${classNames.animate}`}
-      />
-      <div
-        className={`${classNames.comment} ${classNames.br} ${classNames.animate}`}
-      />
-      <div
-        className={`${classNames.comment} ${classNames.br} ${classNames.animate}`}
-      />
+      <div className={`${classNames.title} ${classNames.br} ${classNames.animate}`} />
+      <div className={`${classNames.create} ${classNames.br} ${classNames.animate}`} />
+      <div className={`${classNames.comment} ${classNames.br} ${classNames.animate}`} />
+      <div className={`${classNames.comment} ${classNames.br} ${classNames.animate}`} />
     </div>
   </div>
 );
