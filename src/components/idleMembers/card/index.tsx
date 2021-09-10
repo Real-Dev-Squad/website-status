125e55ad50ae5c58c40b7ffe61b01251e1924cf8
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-09-10 16:10:13 +0530

    Created new variable for dummyProfile and url

--- src/components/idleMembers/card/index.tsx
+++ src/components/idleMembers/card/index.tsx
@@ -1,13 +1,16 @@
 import { FC, useState } from 'react';
 import classNames from '@/components/idleMembers/card/card.module.scss';
 
+const dummyProfile = 'dummyProfile.png';
+const url = process.env.NEXT_PUBLIC_GITHUB_IMAGE_URL;
+
 type Props = {
   idleMemberUserName: string
 }
 
 const Card: FC<Props> = ({ idleMemberUserName }) => {
-  const [assigneeProfilePic, setAssigneeProfilePic] = useState(`${process.env.NEXT_PUBLIC_GITHUB_IMAGE_URL}/${idleMemberUserName}/img.png`);
-  const assigneeImageOnError = () => setAssigneeProfilePic('dummyProfile.png');
+  const [assigneeProfilePic, setAssigneeProfilePic] = useState(`${url}/${idleMemberUserName}/img.png`);
+  const assigneeImageOnError = () => setAssigneeProfilePic(dummyProfile);
   const getMemberDetails = (name: string) => {
     const newWindow = window.open(`https://members.realdevsquad.com/${name}`, '_blank', ' noopener ,norefferrer');
     if (newWindow) newWindow.opener = null;
