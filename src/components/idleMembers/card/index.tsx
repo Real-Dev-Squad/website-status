20956e6bcab33e3cf32cae4c3e1bbc939d9df115
Abhinav Mishra <86315672+abhinav1590@users.noreply.github.com>
2021-09-11 16:44:24 +0530

    changed name to prevent no-para-reassign error

--- src/components/idleMembers/card/index.tsx
+++ src/components/idleMembers/card/index.tsx
@@ -13,7 +13,45 @@ const Card: FC<Props> = ({ idleMemberUserName }) => {
   const getMemberDetails = (name: string) => {
     const newWindow = window.open(`https://members.realdevsquad.com/${name}`, '_blank', ' noopener ,norefferrer');
     if (newWindow) newWindow.opener = null;
+  };import { FC, SyntheticEvent } from 'react';
+import classNames from '@/components/idleMembers/card/card.module.scss';
+import { dummyProfile } from '@/components/constants/display-sections.js';
+
+const imgUrl = process.env.NEXT_PUBLIC_GITHUB_IMAGE_URL;
+
+type Props = {
+  idleMemberUserName: string
+}
+
+const Card: FC<Props> = ({ idleMemberUserName }) => {
+  const assigneeProfilePic = (name: string) => `${imgUrl}/${name}/img.png`;
+  const getMemberDetails = (name: string) => {
+    const newWindow = window.open(`https://members.realdevsquad.com/${name}`, '_blank', ' noopener ,norefferrer');
+    if (newWindow) newWindow.opener = null;
   };
+  const assigneeImageOnError = (e: SyntheticEvent<HTMLImageElement>) => {
+    e.currentTarget.src = dummyProfile;
+  };
+  return (
+    <div
+      className={classNames.card}
+      onClick={() => getMemberDetails(idleMemberUserName)}
+      onKeyDown={() => getMemberDetails(idleMemberUserName)}
+      aria-hidden="true"
+    >
+      <img
+        className={classNames.image}
+        src={assigneeProfilePic(idleMemberUserName)}
+        alt={idleMemberUserName}
+        onError={assigneeImageOnError}
+      />
+      <span className={classNames.name}>{idleMemberUserName}</span>
+    </div>
+  );
+};
+
+export default Card;
+
   const assigneeImageOnError = (events: SyntheticEvent<HTMLImageElement>) => {
     events.currentTarget.src = dummyProfile;
   };
