d2416b0585f8ffa6ac91bb0da0ff0fe4b274e11e
akshay shinde <akshayshinde7289@gmail.com>
2021-09-30 17:58:34 +0530

    using fetch hook

--- src/components/constants/url.js
+++ src/components/constants/url.js
@@ -1,4 +1,3 @@
 export const SUBSCRIBE_TO_CHALLENGE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/challenges/subscribe`;
-
 export const CHALLENGES_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/challenges`;
 export const USER_SELF = `${process.env.NEXT_PUBLIC_BASE_URL}/users/self`;
