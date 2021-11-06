cba6ecb630cae73254d0167b0ba2f8960ae2a463
Shubham <yadav105shubham@gmail.com>
2021-11-06 17:25:23 +0530

    Code standard improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,23 +22,21 @@ const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks`;
 const SELF_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/users/self`;
 const STATUS_ORDER = [ACTIVE, ASSIGNED, COMPLETED, PENDING, UNASSIGNED];
 
-async function updateCardContent(id: string, cardDetails: task, isEditable: boolean) {
-  if (isEditable) {
-    try {
-      const { requestPromise } = fetch({
-        url: `${TASKS_URL}/${id}`,
-        method: 'patch',
-        data: cardDetails,
-      });
-      await requestPromise;
-      toast(SUCCESS, 'Changes have been saved !');
-    } catch (err: any) {
-      if ('response' in err) {
-        toast(ERROR, err.response.data.message);
-        return;
-      }
-      toast(ERROR, err.message);
+async function updateCardContent(id: string, cardDetails: task) {
+  try {
+    const { requestPromise } = fetch({
+      url: `${TASKS_URL}/${id}`,
+      method: 'patch',
+      data: cardDetails,
+    });
+    await requestPromise;
+    toast(SUCCESS, 'Changes have been saved !');
+  } catch (err: any) {
+    if ('response' in err) {
+      toast(ERROR, err.response.data.message);
+      return;
     }
+    toast(ERROR, err.message);
   }
 }
 
@@ -48,9 +46,8 @@ function renderCardList(tasks: task[], isEditable: boolean) {
       content={item}
       key={item.id}
       shouldEdit={isEditable}
-      onContentChange={async (id: string, newDetails: any) => {
-        updateCardContent(id, newDetails, isEditable);
-      }}
+      onContentChange={async (id: string, newDetails: any) => isEditable
+        && updateCardContent(id, newDetails)}
     />
   ));
 }
