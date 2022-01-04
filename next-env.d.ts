52e5d4df29591c29c4a0e89db532936617d594ac
Nikhil Bhandarkar <nikhilbhandarkar1998@gmail.com>
2022-01-04 18:06:43 +0530

    update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-declare module '*.scss';
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
