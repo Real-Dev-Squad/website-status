cad08068e415493fd5bc709ed537e96f40f519e4
brandong512 <brandong512@gmail.com>
2022-01-20 22:37:08 -0800

    add dark mode toggle for tasks, open PRs, stale PRs, add new dark overdue card color

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -46,11 +46,7 @@ const Card: FC<Props> = ({
   );
 
   return (
-<<<<<<< HEAD
     <div className={darkMode ? classNames.darkTheme : classNames.lightTheme}>
-=======
-    <div className={classNames.darkTheme}>
->>>>>>> 28c8c0c (add dark mode for cards in open and stale prs)
       <div
         className={`
           ${classNames.card}
