0c368558fec000d8d348073a31949b27e67252dc
Akanksha Dharkar <akankshadharkar@gmail.com>
2022-01-24 15:20:17 +0530

    Refactoring Navbar and making it responsive

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,17 +1,50 @@
 import Image from 'next/image';
 import styles from '@/components/navBar/navBar.module.scss';
+import React, { useState } from 'react';
 
 const RDSLogo = '/RDSLogo.png';
+const hamburgerLogo = '/hamburger.svg';
 
-const NavBar = () => (
-  <nav className={styles.navBar}>
-    <a className={styles.logo} href="https://realdevsquad.com"><Image width="50px" height="50px" src={RDSLogo} alt="real-dev squad" /></a>
-    <a href="https://welcome.realdevsquad.com/">Welcome</a>
-    <a href="https://www.realdevsquad.com/events.html">Events</a>
-    <a href="https://members.realdevsquad.com/">Members</a>
-    <a href="https://crypto.realdevsquad.com/">Crypto</a>
-    <a className={styles.active} href="https://status.realdevsquad.com/">Status</a>
-  </nav>
-);
+const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+  return (
+  <div className={styles.wrapper}>
+    
+    <nav className={styles.navBar}>
+      <div className={styles.hamburger} onClick={() => setToggle(!toggle)} >
+        <Image src={hamburgerLogo} alt="hamburger-icon" width="25" height="30" />
+      </div>
+
+      <ul className={ toggle ? `${styles.navBarMenu} 
+        ${styles.active}` : 
+        styles.navBarMenu }
+      >
+        <li className={styles.navBarLogoLi}>
+          <a href="https://www.realdevsquad.com/">
+            <img src={RDSLogo} alt="realdevsquad-icon" height="50px" width="50px" />
+          </a>
+        </li>
+        <li className={styles.homeTab}>
+          <a href="https://www.realdevsquad.com/"> Home </a>
+        </li>
+        <li>
+          <a href="https://welcome.realdevsquad.com/"> Welcome </a>
+        </li>
+        <li>
+          <a href="https://www.realdevsquad.com/events.html"> Events </a>
+        </li>
+        <li>
+          <a href="https://members.realdevsquad.com/"> Members </a>
+        </li>
+        <li>
+          <a href="https://crypto.realdevsquad.com/"> Crypto </a>
+        </li>
+        <li>
+          <a className={styles.activeTab} href="https://status.realdevsquad.com/"> Status </a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+)};
 
 export default NavBar;
