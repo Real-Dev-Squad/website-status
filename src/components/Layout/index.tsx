9ccc0738050b73b973bc0e75ba6a40dd8d2e00d1
shashank <shashankkumar2023@gmail.com>
2022-02-14 10:21:12 +0530

    Added buttons to enable admins to edit task

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,10 +1,12 @@
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, useEffect, useRef, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import Footer from '@/components/footer';
 import styles from '@/components/Layout/Layout.module.scss';
 import NavBar from '@/components/navBar';
-
+import Image from 'next/image';
+import { toast, ToastTypes } from '@/helperFunctions/toast';
+import fetch from '@/helperFunctions/fetch';
 interface Props {
   children?: ReactNode
 }
@@ -18,14 +20,40 @@ const navBarContent = (title: string, refUrl: string, isActive: boolean = false)
     </Link>
   );
 };
-
+const { SUCCESS, ERROR } = ToastTypes;
+const SELF_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/users/self`;
 const Layout: FC<Props> = ({ children }) => {
+  const [IsUserAuthorized, setIsUserAuthorized] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { requestPromise } = fetch({ url: SELF_URL });
+        const { data } = await requestPromise;
+        const userRoles = {
+          adminUser: data.roles?.admin,
+          superUser: data.roles?.super_user,
+        };
+        const { adminUser, superUser } = userRoles;
+        setIsUserAuthorized(!!adminUser || !!superUser);
+      } catch (err: any) {
+        toast(ERROR, err.message);
+      }
+    };
+    fetchData();
+
+    return (() => {
+      setIsUserAuthorized(false);
+    });
+  }, []);
+
   const router = useRouter();
 
   // Dev feature toggle
   const { query } = router;
-  const dev = !!query.dev;
-
+  let dev = !!query.dev;
+  let elementRef  = useRef();
+  
   return (
     <div className={styles.layout}>
       <NavBar />
@@ -42,6 +70,7 @@ const Layout: FC<Props> = ({ children }) => {
           {navBarContent('Stale PRs', '/stale-pr', router.pathname === '/stale-pr')}
           |
           {navBarContent('Idle Members', '/idle-members', router.pathname === '/idle-members')}
+          
           {
             (dev)
             && (
@@ -51,7 +80,25 @@ const Layout: FC<Props> = ({ children }) => {
               </>
             )
           }
+          {IsUserAuthorized && <Link href={{ pathname: '/', query: { dev: 'true' } }} passHref><Image
+                height= "25px"
+                width="25px"
+                src="/add.png"
+                alt="add"
+            /></Link>}  
         </div>
+        {IsUserAuthorized && <div className={styles.edit} >
+          <Link href={{ pathname: '/', query: { edit: 'true' } }} passHref>
+            <Image
+                className={styles.edit}
+                height= "25px"
+                width="25px"
+                src="/pencil.png"
+                alt="pencil"
+            />
+          </Link>
+        </div>}
+        
         {children}
       </div>
       <Footer />
