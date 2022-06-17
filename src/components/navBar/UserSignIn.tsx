0eca3c72c4c46db296a17d414349e49e68353a37
rohan09-raj <rajrohan1914@gmail.com>
2022-06-17 17:55:26 +0530

    Refactored UserSignIn component

--- src/components/navBar/UserSignIn.tsx
+++ src/components/navBar/UserSignIn.tsx
@@ -11,40 +11,41 @@ const UserSignIn: FC<SignInButton> = ({ btnText, userData, loggedIn }) => {
 
   const { firstName, profilePicture } = userData;
 
-  return !loggedIn ? (
+  return (
     <button
-      className={styles.btnLogin}
+      className={loggedIn ? styles.userGreet : styles.btnLogin}
       onClick={() =>
-        (location.href =
-          "https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97")
+        (location.href = loggedIn
+          ? "https://my.realdevsquad.com/"
+          : "https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97")
       }
       type="button"
     >
-      <span className={styles.btnLoginText}>{btnText}</span>
-      <Image
-        className={styles.githubLogo}
-        src={GITHUB_LOGO}
-        alt="GitHub_Icon"
-        height={EIGHTEEN_PX}
-        width={EIGHTEEN_PX}
-      />
-    </button>
-  ) : (
-    <button
-      onClick={() => (location.href = "https://my.realdevsquad.com/")}
-      type="button"
-      className={styles.userGreet}
-    >
-      <div className={styles.userGreetMsg}>
-        Hello, {firstName ? firstName : `User`}!
-      </div>
-      <Image
-        className={styles.userProfilePic}
-        src={profilePicture || DEFAULT_AVATAR}
-        alt="User_Profile_Picture"
-        width={THIRTY_TWO_PX}
-        height={THIRTY_TWO_PX}
-      />
+      {loggedIn ? (
+        <>
+          <div className={styles.userGreetMsg}>
+            Hello, {firstName ? firstName : `User`}!
+          </div>
+          <Image
+            className={styles.userProfilePic}
+            src={profilePicture || DEFAULT_AVATAR}
+            alt="User_Profile_Picture"
+            width={THIRTY_TWO_PX}
+            height={THIRTY_TWO_PX}
+          />
+        </>
+      ) : (
+        <>
+          <span className={styles.btnLoginText}>{btnText}</span>
+          <Image
+            className={styles.githubLogo}
+            src={GITHUB_LOGO}
+            alt="GitHub_Icon"
+            height={EIGHTEEN_PX}
+            width={EIGHTEEN_PX}
+          />
+        </>
+      )}
     </button>
   );
 };
