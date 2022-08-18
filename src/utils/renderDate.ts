e25fa4f4b66a2dd925407e7f5a388f458d7e63a0
akshay shinde <akshayshinde7289@gmail.com>
2022-08-18 20:57:08 +0530

    minor changes

--- src/utils/renderDate.ts
+++ src/utils/renderDate.ts
@@ -1,7 +1,7 @@
 const moment = require('moment');
+// dates are in number and string format.
 function dateFromNow(date: string | number): string {
   if (date === null) return 'Invalid date';
-  // dates are in number and string format in database.
   const dateInNumber = Number(date);
   const formatDate = isNaN(dateInNumber) ?  new Date(date) : new Date(dateInNumber * 1000);
   return moment(formatDate).fromNow();
