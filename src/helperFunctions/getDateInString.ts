358a4141f22783dc69e1a58e9127513df9d293df
DeepanshuSarswat <sarswatdeepanshu@gmail.com>
2022-08-20 05:10:34 +0530

    date format issue fixed

--- src/helperFunctions/getDateInString.ts
+++ src/helperFunctions/getDateInString.ts
@@ -1,7 +1,7 @@
 const getDateInString = (date: Date) => {
     const year = date.getFullYear()
-    const month = ("0" + (date.getMonth() + 1)).slice(-2);
-    const day = ("0" + date.getDate()).slice(-2);
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
     const strDate = [year, month, day].join('-');
     return strDate;
 };
