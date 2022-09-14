91704e5ffb1b0088fd71e33300e137d32a69dca1
Muralidhar22 <muralidhar7794@gmail.com>
2022-09-14 22:02:54 +0530

    fix: user.types properties added

--- src/interfaces/user.type.ts
+++ src/interfaces/user.type.ts
@@ -1,9 +1,28 @@
 type userType = {
-  github_display_name: String,
-  username: string,
-  github_id: string,
   id: string,
+  username?: string,
+  github_id: string,
+  github_display_name: String | null,
   incompleteUserDetails: boolean,
+  roles: {
+    archived: boolean,
+    admin?: boolean,
+    super_user?: boolean,
+  }
+  isMember: boolean,
+  status?: string,
+  picture?: {
+    publicId: string,
+    url: URL,
+  }
+  first_name?: string,
+  last_name?: string,
+  company?: string,
+  yoe?: number,
+  designation?: string,
+  instagram_id?: string | URL,
+  twitter_id?: string | URL,
+  linkedin_id?: string | URL
 };
 
 export default userType;
