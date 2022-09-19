b461cb89325173a8784c6b72fe9fd036619bd2bc
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2022-09-19 20:13:13 +0530

    login feature

--- src/components/constants/url.js
+++ src/components/constants/url.js
@@ -1,4 +1,6 @@
 const BASE_URL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
 export const SUBSCRIBE_TO_CHALLENGE_URL = `${BASE_URL}/challenges/subscribe`;
+export const LOGIN_URL = 'https://github.com/login/oauth/authorize?client_id=23c78f66ab7964e5ef97';
+export const USER_PROFILE_URL = 'https://my.realdevsquad.com/profile';
 export const CHALLENGES_URL = `${BASE_URL}/challenges`;
 export const USER_SELF = `${BASE_URL}/users/self`;
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -1,17 +1,75 @@
+import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '@/components/navBar/navBar.module.scss';
-
+import { LOGIN_URL, USER_PROFILE_URL, USER_SELF } from '@/components/constants/url'
+import Link from 'next/link';
 const RDSLogo = '/RDSLogo.png';
 
-const NavBar = () => (
-  <nav className={styles.navBar}>
-    <a className={styles.logo} href="https://realdevsquad.com"><Image width="45px" height="45px" src={RDSLogo} alt="real-dev squad" /></a>
-    <a href="https://welcome.realdevsquad.com/">Welcome</a>
-    <a href="https://www.realdevsquad.com/events.html">Events</a>
-    <a href="https://members.realdevsquad.com/">Members</a>
-    <a href="https://crypto.realdevsquad.com/">Crypto</a>
-    <a className={styles.active} href="https://status.realdevsquad.com/">Status</a>
-  </nav>
-);
+const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const GITHUB_LOGO = '/github-white.png';
+  const DEFAULT_AVATAR = '/Avatar.png';
+  const [userData, setUserData] = useState({
+    userName: '',
+    firstName: '',
+    profilePicture: ''
+  });
+
+ 
+
+  return (
+    <nav className={styles.navBar}>
+      <div>
+        <a className={styles.logo} href="https://realdevsquad.com"><Image width="45px" height="45px" src={RDSLogo} alt="real-dev squad" /></a>
+        <a href="https://welcome.realdevsquad.com/">Welcome</a>
+        <a href="https://www.realdevsquad.com/events.html">Events</a>
+        <a href="https://members.realdevsquad.com/">Members</a>
+        <a href="https://crypto.realdevsquad.com/">Crypto</a>
+        <a className={styles.active} href="https://status.realdevsquad.com/">Status</a>
+      </div>
+
+      <div>
+        {!isLoggedIn && (
+          <Link href={LOGIN_URL}>
+            <a className={styles.btnLogin}>
+              <button type="button" className={styles.btnLoginText}>
+                Sign In With GitHub
+                <img
+                  className={styles.githubLogo}
+                  src={GITHUB_LOGO}
+                  alt="GitHub Icon"
+                  height="15px"
+                  width="15px"
+                />
+              </button>
+            </a>
+          </Link>
+        )}
+        {isLoggedIn && (
+          <div className={styles.userGreet}>
+            <Link href={USER_PROFILE_URL}>
+              <a>
+                <div className={styles.userGreetMsg}>
+                  {isLoggedIn
+                    ? `Hello, ${userData.firstName}!`
+                    : `Hello, User!`}
+                </div>
+                <img
+                  className={styles.userProfilePic}
+                  src={
+                    isLoggedIn
+                      ? `${userData.profilePicture}`
+                      : `${DEFAULT_AVATAR}`
+                  }
+                  alt="Profile Pic"
+                />
+              </a>
+            </Link>
+          </div>
+        )}
+      </div>
+    </nav>
+  )
+};
 
 export default NavBar;
