5367b0b45a956750165fe0a2e7e72e82cbf6c44c
Sonu Kumar <bsonu.saw@gmail.com>
2022-10-08 11:18:55 +0530

    ran lint

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -1,44 +1,43 @@
-import { createContext, ReactNode, useContext, FC, useState } from "react";
-import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, FC, useState } from 'react'
+import { useRouter } from 'next/router';
 
 interface Props {
-	children?: ReactNode;
+    children?: ReactNode;
 }
 
 export type ContextType = {
-	state: { isEditMode: boolean };
-	actions: { onEditRoute: () => void };
-};
-const AppContext = createContext<ContextType | null>(null);
+    state: { isEditMode: boolean };
+    actions: { onEditRoute: () => void }
+}
+const AppContext = createContext<ContextType | null>(null)
 
 const AppWrapperContext: FC<Props> = ({ children }) => {
-	const router = useRouter();
-	const { query } = router;
-	const { edit: editQuery } = query;
-	const isEditMode = editQuery === "true";
+    const router = useRouter()
+    const { query } = router;
+    const { edit: editQuery } = query;
+    const isEditMode = editQuery === 'true';
 	const onEditRoute = () => {
-		router.push({ query: "edit=true" }, undefined, { shallow: true }); //TODO handle query better https://github.com/Real-Dev-Squad/website-status/issues/299
-	};
-	return (
-		<AppContext.Provider
-			value={{
-				state: {
-					isEditMode,
-				},
-				actions: {
-					onEditRoute,
-				},
-			}}>
-			{children}
-		</AppContext.Provider>
-	);
-};
+		router.push({ query: 'edit=true' }, undefined, { shallow: true }); //TODO handle query better https://github.com/Real-Dev-Squad/website-status/issues/299
+    }
+    return (
+        <AppContext.Provider value={{
+            state: {
+                isEditMode
+            },
+            actions: {
+                onEditRoute
+            }
+        }}>
+            {children}
+        </AppContext.Provider>
+    )
+}
 
 export const useAppContext = () => {
-	const context = useContext(AppContext) as ContextType;
-	if (context === undefined) {
-		throw new Error("useAppContext must be used within a CountProvider");
-	}
-	return context || {};
-};
-export default AppWrapperContext;
+    const context = useContext(AppContext) as ContextType
+    if (context===undefined) {
+        throw new Error('useAppContext must be used within a CountProvider')
+    }
+    return context || {}
+}
+export default AppWrapperContext
