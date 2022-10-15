ff8c588ee9169271ee8bce47c2e5f23d557ae2ca
Codewithme-25 <paltandekar.svg@gmail.com>
2022-10-15 13:09:01 +0530

    commit

--- src/components/tasks/card/TaskDetails.js
+++ src/components/tasks/card/TaskDetails.js
@@ -15,58 +15,60 @@ function TaskDetails({
   return (
     <>
       <NavBar />
-      <main className={classNames.container}>
-        <h1>{title}</h1>
-        <div className={classNames.btn1}>
-          <button type="button1" className={classNames.btn1}>
+      <main className={classNames['container']}>
+      <div className={classNames['head']}>
+         <h1 className={classNames['head1']}>{title} </h1> 
+        <div className={classNames['btn1']}>
+          <button type="button1" className={classNames['btn1']}>
             Edit
           </button>
         </div>
-
-        <div className={classNames.mainblock}>
-          <div className={classNames.Description}>
-            <h5>Descritpion</h5>
-            <p>
-              In React, all DOM properties and attributes (including event
-              handlers) should be camelCased. For example, the HTML attribute
-              tabindex corresponds to the attribute tabIndex in React. The
-              exception is aria-* and data-* attributes, which should be
-              lowercased. For example, you can keep aria-label as aria-label.
-            </p>
+        </div>
+        <div className={classNames['mainblock']}>
+          <div className={classNames['description']}>
+            <h5  className={classNames['head5']}>Descritpion</h5>
+            <p className={classNames['para']}>
+            In React, all DOM properties and attributes 
+            (including event handlers) should be camelCased.
+             For example, the HTML attribute tabindex corresponds 
+             to the attribute tabIndex in React. The exception
+              is aria-* and data-* attributes, which should be 
+              lowercased For example, you can keep aria-label as aria-label.
+              </p>
           </div>
-          <div className={classNames.Participants}>
+          <div className={classNames['participants']}>
             <img
               src="./public/participant logo.png"
-              className={classNames.logo}
+              className={classNames['logo']}
               alt="logo"
             ></img>
-            <h5>Participants</h5>
-            <div className={classNames['part-opt']}>
-              <h6>Assignee:</h6>
-              <h6>Reporter:</h6>
+            <h5  className={classNames['head5']}>Participants</h5>
+            <div className={classNames['partopt']}>
+              <h6  className={classNames['head6']}>Assignee:</h6>
+              <h6  className={classNames['head6']}>Reporter:</h6>
             </div>
           </div>
-          <div className={classNames.Details}>
-            <h5>Details</h5>
-            <div className={classNames['detail-opt']}>
-              <h6>Type: </h6>
-              <h6>Priority: </h6>
-              <h6>Resolution: </h6>
-              <h6>Status: </h6>
-              <h6>label: </h6>
-              <h6>link: </h6>
+          <div className={classNames['details']}>
+            <h5  className={classNames['head5']}>Details</h5>
+            <div className={classNames['detailopt']}>
+              <h6  className={classNames['head6']}>Type: </h6>
+              <h6  className={classNames['head6']}>Priority: </h6>
+              <h6  className={classNames['head6']}>Resolution: </h6>
+              <h6  className={classNames['head6']}>Status: </h6>
+              <h6  className={classNames['head6']}>label: </h6>
+              <h6  className={classNames['head6']}>link: </h6>
             </div>
           </div>
-          <div className={classNames.Dates}>
-            <div className={classNames['date-opt']}>
-              <h5>Dates</h5>
-              <h6>Created at :</h6>
-              <h6>Completed at :</h6>
-              <h6>Updated at :</h6>
+          <div className={classNames['dates']}>
+            <div className={classNames['dateopt']}>
+              <h5  className={classNames['head5']}>Dates</h5>
+              <h6  className={classNames['head6']}>Created at :</h6>
+              <h6  className={classNames['head6']}>Completed at :</h6>
+              <h6  className={classNames['head6']}>Updated at :</h6>
             </div>
           </div>
-          <div className={classNames.Activity}>
-            <h5>Activity</h5>
+          <div className={classNames['activity']}>
+            <h5  className={classNames['head5']}>Activity</h5>
             <div className={classNames.btn}>
               <button type="button2" className="btn btn-light mr-1">
                 All
