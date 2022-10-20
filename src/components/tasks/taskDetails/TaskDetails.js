6eb5b060347d3967df19d72539803d9c9d7af903
YashJain24 <75324135+YashJain24-chief@users.noreply.github.com>
2022-10-20 18:33:25 +0530

    Built the edit functionality for super user

--- src/components/tasks/taskDetails/TaskDetails.js
+++ src/components/tasks/taskDetails/TaskDetails.js
@@ -3,76 +3,198 @@ import NavBar from '@/components/navBar/index.tsx';
 import TaskContainer from './TaskContainer';
 import Details from './Details';
 import classNames from './task-details.module.scss';
+import { useState, useEffect } from 'react';
+import useFetch from '@/hooks/useFetch';
 
-function TaskDetails({
-  title,
-  purpose,
-  type,
-  priority,
-  status,
-  featureUrl,
-  participants,
-  assignee,
-  startedOn,
-  endsOn,
-}) {
+function TaskDetails({ url, taskID }) {
+  const isAuthorized = true; //use context when Akshay's PR is merged
+
+  const [isEdit, setIsEdit] = useState(false);
+
+  const [taskDetails, setTaskDetails] = useState({});
+  const [editedDetails, setEditedDetials] = useState({});
+  const { response, error, isLoading } = useFetch(url);
+
+  useEffect(() => {
+    setTaskDetails({
+      ...response.taskData,
+      initialData: { ...response.taskData },
+    });
+  }, [isLoading, response]);
+
   function convertTimeStamp(timeStamp) {
     const dateTime = new Date(timeStamp);
     return dateTime.toLocaleString();
   }
 
+  function handleChange(event) {
+    setEditedDetials((prv) => {
+      return {
+        ...prv,
+        [event.target.name]: event.target.value,
+      };
+    });
+    setTaskDetails((prv) => {
+      return {
+        ...prv,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+
+  function onCancel() {
+    setIsEdit(false);
+    setEditedDetials({});
+    setTaskDetails((prv) => {
+      return {
+        ...prv.initialData,
+        initialData: prv.initialData,
+      };
+    });
+  }
+
+  async function onSave() {
+    setIsEdit(false);
+    setTaskDetails((prv) => {
+      return {
+        ...prv,
+        initialData: { ...prv },
+      };
+    });
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/tasks/${taskID}`,
+        {
+          method: 'PATCH',
+          body: JSON.stringify(editedDetails),
+          credentials: 'include',
+          headers: {
+            'Content-type': 'application/json',
+          },
+        }
+      );
+      if (response.ok) {
+        setEditedDetials({});
+      }
+    } catch (err) {
+      console.log(err); //error is not being caught
+    }
+  }
+
   return (
     <>
       <NavBar />
-      <div className={classNames['parent_container']}>
-        <div className={classNames['title_container']}>
-          <h4 className={classNames['title']}>{title}</h4>
-          <button type="button" className={classNames['edit_btn']}>
-            Edit
-          </button>
-        </div>
-
-        <div className={classNames['details_container']}>
-          <div className={classNames['details_left_container']}>
-            <TaskContainer block_title="Description" hasImg={false}>
-              <p className={classNames['block_content']}>
-                {purpose === undefined ? 'No description available' : purpose}
-              </p>
-            </TaskContainer>
-            <TaskContainer block_title="Details" hasImg={false}>
-              <div className={classNames['sub_details_container']}>
-                <Details detailType={'Type'} value={type} />
-                <Details detailType={'Priority'} value={priority} />
-                <Details detailType={'Status'} value={status} />
-                <Details detailType={'Link'} value={featureUrl} />
-              </div>
-            </TaskContainer>
-            <TaskContainer block_title="Activity" hasImg={false} />
-          </div>
-
-          <div className={classNames['details_right_container']}>
-            <TaskContainer
-              src="/participant_logo.png"
-              block_title="Participants"
-              hasImg={true}
-            >
-              <Details detailType={'Assignee'} value={assignee} />
-              <Details detailType={'Reporter'} value={'Ankush'} />
-            </TaskContainer>
-            <TaskContainer
-              src="/calendar-icon.png"
-              block_title="Dates"
-              hasImg={true}
-            >
-              <Details
-                detailType={'StartedOn'}
-                value={convertTimeStamp(startedOn)}
+      {!!error && <p>Something went wrong!</p>}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <div className={classNames['parent_container']}>
+          <div className={classNames['title_container']}>
+            {!isEdit ? (
+              <h4 className={classNames['title']}>{taskDetails?.title}</h4>
+            ) : (
+              <textarea
+                className={classNames['textarea']}
+                name="title"
+                value={taskDetails?.title}
+                onChange={(event) => handleChange(event)}
               />
-              <Details detailType={'EndsOn'} value={convertTimeStamp(endsOn)} />
-            </TaskContainer>
+            )}
+            {isAuthorized && !isEdit && (
+              <button
+                type="button"
+                className={classNames['edit_btn']}
+                onClick={() => setIsEdit(true)}
+              >
+                Edit
+              </button>
+            )}
+            {isEdit && (
+              <div className={classNames['isEditTrue']}>
+                <button
+                  type="button"
+                  className={classNames['edit_btn']}
+                  onClick={onCancel}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className={classNames['edit_btn']}
+                  onClick={onSave}
+                >
+                  Save
+                </button>
+              </div>
+            )}
+          </div>
+
+          <div className={classNames['details_container']}>
+            <div className={classNames['details_left_container']}>
+              <TaskContainer block_title="Description" hasImg={false}>
+                {!isEdit ? (
+                  <p className={classNames['block_content']}>
+                    {taskDetails?.purpose === undefined ||
+                    taskDetails?.purpose === ''
+                      ? 'No description available'
+                      : taskDetails?.purpose}
+                  </p>
+                ) : (
+                  <textarea
+                    className={classNames['textarea']}
+                    name="purpose"
+                    value={taskDetails?.purpose}
+                    onChange={(event) => handleChange(event)}
+                  />
+                )}
+              </TaskContainer>
+              <TaskContainer block_title="Details" hasImg={false}>
+                <div className={classNames['sub_details_container']}>
+                  <Details detailType={'Type'} value={taskDetails?.type} />
+                  <Details
+                    detailType={'Priority'}
+                    value={taskDetails?.priority}
+                  />
+                  <Details detailType={'Status'} value={taskDetails?.status} />
+                  <Details
+                    detailType={'Link'}
+                    value={taskDetails?.featureUrl}
+                  />
+                </div>
+              </TaskContainer>
+              <TaskContainer block_title="Activity" hasImg={false} />
+            </div>
+
+            <div className={classNames['details_right_container']}>
+              <TaskContainer
+                src="/participant_logo.png"
+                block_title="Participants"
+                hasImg={true}
+              >
+                <Details
+                  detailType={'Assignee'}
+                  value={taskDetails?.assignee}
+                />
+                <Details detailType={'Reporter'} value={'Ankush'} />
+              </TaskContainer>
+              <TaskContainer
+                src="/calendar-icon.png"
+                block_title="Dates"
+                hasImg={true}
+              >
+                <Details
+                  detailType={'StartedOn'}
+                  value={convertTimeStamp(taskDetails?.startedOn)}
+                />
+                <Details
+                  detailType={'EndsOn'}
+                  value={convertTimeStamp(taskDetails?.endsOn)}
+                />
+              </TaskContainer>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
--- src/pages/tasks/[id].js
+++ src/pages/tasks/[id].js
@@ -1,7 +1,4 @@
 import { useRouter } from 'next/router';
-import useFetch from '@/hooks/useFetch';
-import { useState } from 'react';
-import { useEffect } from 'react';
 
 import TaskDetails from '@/components/tasks/taskDetails/TaskDetails';
 
@@ -10,31 +7,9 @@ const TaskDetailsPage = () => {
   const { id } = router.query;
   const TASK_DETAILS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks/${id}/details`;
 
-  const [taskDetails, setTaskDetails] = useState(null);
-  const { response, error, isLoading } = useFetch(TASK_DETAILS_URL);
-
-  useEffect(() => {
-    setTaskDetails(response.taskData);
-  }, [isLoading, response]);
   return (
     <>
-      {!!error && <p>Something went wrong!</p>}
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <TaskDetails
-          title={taskDetails?.title}
-          purpose={taskDetails?.purpose}
-          type={taskDetails?.type}
-          priority={taskDetails?.priority}
-          status={taskDetails?.status}
-          links={taskDetails?.featureUrl}
-          participants={taskDetails?.participants}
-          assignee={taskDetails?.assignee}
-          startedOn={taskDetails?.startedOn}
-          endsOn={taskDetails?.endsOn}
-        />
-      )}
+      <TaskDetails url={TASK_DETAILS_URL} taskID={id} />
     </>
   );
 };
