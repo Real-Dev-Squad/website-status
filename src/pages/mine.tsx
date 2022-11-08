841cc62c4fe8e0abf7b48c3543f60068df4ca299
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-11-08 19:32:54 +0530

    Feat: Edit button added with required functionality
    Now members can have edit access for task- title, status, progress in the mine tab of status site.

--- src/pages/mine.tsx
+++ src/pages/mine.tsx
@@ -1,28 +1,56 @@
-import { FC, useState, useEffect } from 'react';
+import { FC, useState, useEffect, useContext } from 'react';
 import Head from '@/components/head';
 import Layout from '@/components/Layout';
 import Card from '@/components/tasks/card';
 import useFetch from '@/hooks/useFetch';
 import classNames from '@/styles/tasks.module.scss';
 import task from '@/interfaces/task.type';
+import { toast, ToastTypes } from '@/helperFunctions/toast';
+import { useAppContext } from '@/context';
+import { isUserAuthorizedContext } from '@/context/isUserAuthorized';
+import fetch from '@/helperFunctions/fetch';
 
 const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks/self`;
+const { SUCCESS, ERROR } = ToastTypes;
 
-function CardList(tasks: task[]) {
+async function updateCardContent(id: string, cardDetails: task) {
+  try {
+    const { requestPromise } = fetch({
+      url: `${TASKS_URL}/${id}`,
+      method: 'patch',
+      data: cardDetails,
+    });
+    await requestPromise;
+    toast(SUCCESS, 'Changes have been saved !');
+  } catch (err: any) {
+    if ('response' in err) {
+      toast(ERROR, err.response.data.message);
+      return;
+    }
+    toast(ERROR, err.message);
+  }
+}
+
+function CardList(tasks: task[], isEditable: boolean) {
   return tasks.map(
     (item: task) => (
       <Card
         content={item}
         key={item.id}
-        shouldEdit={false}
-        onContentChange={undefined}
+        shouldEdit={isEditable}
+        onContentChange={async (id: string, newDetails: any) => isEditable
+          && updateCardContent(id, newDetails)}
       />
     ),
   );
 }
 
 const Mine: FC = () => {
+  const { state: appState } = useAppContext();  
   const [tasks, setTasks] = useState<task[]>([]);
+  const { isEditMode } = appState;
+  const isUserAuthorized = useContext(isUserAuthorizedContext);
+  const isEditable = isEditMode;
   const {
     response,
     error,
@@ -59,7 +87,7 @@ const Mine: FC = () => {
                   tasks.length > 0
                     ? (
                       <div>
-                        {CardList(tasks)}
+                        {CardList(tasks, isEditable)}
                       </div>
                     ) : (!error && 'No Tasks Found')
                 }
