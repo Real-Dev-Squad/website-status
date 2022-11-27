7a0888c0d14ebdc879160416d794576beaa4c348
Ranita Saha <saharanitaa1230@gmail.com>
2022-11-27 20:36:12 +0530

    fixed the future dependency

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
- const nextConfig = {
+module.exports = {
   images: {
     domains: ["raw.githubusercontent.com", "res.cloudinary.com"],
   },
-}
-
-module.exports = nextConfig
-
+};
