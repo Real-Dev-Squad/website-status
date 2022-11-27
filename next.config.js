d5bd394505a259b8701b23578fd79c505c11ac5a
Ranita Saha <saharanitaa1230@gmail.com>
2022-11-27 20:17:04 +0530

    removed the future dependency

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@
  * @type {import('next').NextConfig}
  */
  const nextConfig = {
-  env: {
-    webpack5: true,
-  },
   images: {
     domains: ["raw.githubusercontent.com", "res.cloudinary.com"],
   },
