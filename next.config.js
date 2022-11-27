b78985154f7cb5dad369670c45ac4de7f5dd635c
Ranita Saha <saharanitaa1230@gmail.com>
2022-11-27 16:18:12 +0530

    fix: resolved warning

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
-module.exports = {
-  future: {
+/**
+ * @type {import('next').NextConfig}
+ */
+ const nextConfig = {
+  env: {
     webpack5: true,
   },
   images: {
     domains: ["raw.githubusercontent.com", "res.cloudinary.com"],
   },
-};
+}
+
+module.exports = nextConfig
+
