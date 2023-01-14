a44b2a8ea792a9acf2419e65bd4f5b13d822448b
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2023-01-14 21:17:30 +0530

    refactor

--- __tests__/Unit/Components/IdleUsers/Card.test.tsx
+++ __tests__/Unit/Components/IdleUsers/Card.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Card from '@/components/idleUsers/card';
 import { MEMBERS_URL } from '@/components/constants/url';
 import { IdleUser } from '@/interfaces/idleUser.type';
