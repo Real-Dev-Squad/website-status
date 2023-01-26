cf589e410d3bdfa59e35e4e417202c0f17fa779f
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2023-01-26 05:35:26 +0530

    Will fix CSS if the name is long

--- src/components/idleUsers/card/index.tsx
+++ src/components/idleUsers/card/index.tsx
@@ -16,7 +16,8 @@ const Card: FC<Props> = ({ user }) => {
   const profileUrl = `${MEMBERS_URL}/${user.username}`
 
   return (
-      <a data-testid='profile-card' className={styles.card} href={profileUrl} > 
+    <>
+      <a data-testid='profile-card' className={styles.card} href={profileUrl} >
         <Image
           src={userImg || DUMMY_PROFILE}
           alt={user.full_name}
@@ -27,7 +28,20 @@ const Card: FC<Props> = ({ user }) => {
         />
         <p className={styles.name}>{user.full_name}</p>
         <span data-testid='idle-since'>{idleSinceText}</span>
-      </a> 
+      </a>
+      <a data-testid='profile-card' className={styles.card} href={profileUrl} >
+        <Image
+          src={userImg || DUMMY_PROFILE}
+          alt={user.full_name}
+          width={150}
+          height={150}
+          data-testid='user-image'
+          priority={true}
+        />
+        <p className={styles.name}>{user.full_name}</p>
+        <span data-testid='idle-since'>{idleSinceText}</span>
+      </a>
+    </>
   );
 };
 
