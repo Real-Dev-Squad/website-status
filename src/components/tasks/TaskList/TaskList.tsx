41125db371afb939d6eabdb30d0b75cc98c9fe2d
shubham bajaj <shmbajaj32@gmail.com>
2023-01-29 02:27:37 +0530

    refactor: added constants and helper function

--- src/components/tasks/TaskList/TaskList.tsx
+++ src/components/tasks/TaskList/TaskList.tsx
@@ -3,6 +3,7 @@ import task from "@/interfaces/task.type";
 import { useState } from "react";
 import Card from "../card";
 import styles from "../card/card.module.scss";
+import {SEE_MORE, INITIAL_TASKS_LIMIT, ADD_MORE_TASKS_LIMIT} from "../constants";
 
 type TaksListProps = {
   tasks: task[];
@@ -11,24 +12,25 @@ type TaksListProps = {
   updateCardContent?: (id: string, cardDetails: task) => void;
 };
 
+function getFilteredTasks({hasLimit, tasksLimit, tasks}: {hasLimit: boolean, tasksLimit: number, tasks: task[]}) {
+  if (!hasLimit) return tasks;
+  return tasks.slice(0, tasksLimit);
+}
+
 export default function TaskList({
   tasks,
   updateCardContent,
   isEditable = false,
   hasLimit = false,
 }: TaksListProps) {
-  const initialTasksLimit = hasLimit ? 3 : tasks.length;
+  const initialTasksLimit = hasLimit ? INITIAL_TASKS_LIMIT : tasks.length;
   const beautifiedTasks = beautifyTaskStatus(tasks);
   const [tasksLimit, setTasksLimit] = useState<number>(initialTasksLimit);
-  const filteredTasks = getFilteredTasks();
-  function getFilteredTasks() {
-    if (!hasLimit) return beautifiedTasks;
-    return beautifiedTasks.slice(0, tasksLimit);
-  }
+  const filteredTasks = getFilteredTasks({tasks: beautifiedTasks, hasLimit, tasksLimit});
   function onSeeMoreTasksHandler() {
-    setTasksLimit((prevLimit) => prevLimit + 5);
+    setTasksLimit((prevLimit) => prevLimit + ADD_MORE_TASKS_LIMIT);
   }
-
+  
   return (
     <>
       {filteredTasks.map((item: task) => (
@@ -47,7 +49,7 @@ export default function TaskList({
           onClick={onSeeMoreTasksHandler}
           className={styles.seeMoreTasks}
         >
-          See More
+          {SEE_MORE}
         </button>
       )}
     </>
