64c36ca45b334cf6f6f656d2acb2731f47aa1851
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2023-02-20 21:53:35 +0530

    sort imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
 import type { AppProps } from 'next/app';
 import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import '@/styles/index.scss';
 import AppWrapperContext from '@/context';
 import IsUserAuthorizedContext from '@/context/isUserAuthorized';
 import { Provider } from 'react-redux';
 import { ApiProvider } from '@reduxjs/toolkit/dist/query/react';
 import { statusApi } from 'slices/apiSlice';
 import { store } from 'store';
+import 'react-toastify/dist/ReactToastify.css';
+import '@/styles/index.scss';
 
 if (process.env.NEXT_PUBLIC_API_MOCKING === 'ON') {
   require('../../__mocks__/mswInit');
