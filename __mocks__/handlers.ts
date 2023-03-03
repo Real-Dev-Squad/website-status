41c2e96c32c5f9fba6a20d4eecef6f953dfa0978
Mahima Khandelwal <mahi.khandelwal28@gmail.com>
2023-03-03 19:03:31 +0530

    revert .env.development changes

--- __mocks__/handlers.ts
+++ __mocks__/handlers.ts
@@ -1,7 +1,9 @@
 import taskHandlers from "./handlers/tasks.handler";
 import selfHandler from "./handlers/self.handler";
 import idleUserHandler from './handlers/idle-users.handler';
+import tagsHandler from './handlers/tags.handler';
+import levelsHandler from './handlers/levels.handler'
 
-const handlers = [...taskHandlers, ...selfHandler, ...idleUserHandler];
+const handlers = [...taskHandlers, ...selfHandler, ...idleUserHandler, ...tagsHandler, ...levelsHandler];
 
 export default handlers;
