fa40849470724ffc768aef0bcbb9e1bb388b50ad
shubham bajaj <shmbajaj32@gmail.com>
2023-03-10 01:50:53 +0530

    feat: testing useGetUsersQuery hook

--- __tests__/Unit/hooks/usersApi.test.tsx
+++ __tests__/Unit/hooks/usersApi.test.tsx
@@ -1,47 +1,42 @@
-
 import { setupServer } from 'msw/node';
-import { useGetUsersQuery, useGetUsersByLinkQuery, useGetUsersByUsernameQuery } from '@/app/services/usersApi';
-import { usersHandler } from "../../../__mocks__/handlers/users.handler";
-
-
-import React, { PropsWithChildren } from 'react'
-import { renderHook } from '@testing-library/react'
-import type { RenderOptions } from '@testing-library/react'
-import type { PreloadedState } from '@reduxjs/toolkit'
-import { Provider } from 'react-redux'
-import { setupStore } from '@/app/store'
-import type { AppStore,RootState } from '@/app/store'
-
-interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
-  preloadedState?: PreloadedState<RootState>
-  store?: AppStore
-}
+import { useGetUsersQuery } from '@/app/services/usersApi';
+import { usersHandler } from '../../../__mocks__/handlers/users.handler';
+import React, { PropsWithChildren } from 'react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { Provider } from 'react-redux';
+import { setupStore } from '@/app/store';
+import fetchMock from 'jest-fetch-mock';
 
 const server = setupServer(...usersHandler);
 
-beforeAll(() => server.listen());
+beforeAll(() => {
+  server.listen();
+  fetchMock.enableMocks();
+});
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
-
-export function renderWithProviders<P, R>(
-  callback: (props: P) => R,
-  {
-    preloadedState = {},
-    store = setupStore(preloadedState),
-    ...renderOptions
-  }: ExtendedRenderOptions = {}
-) {
-  function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
-    return <Provider store={store}>{children}</Provider>
-  }
-  return { store, ...renderHook(callback, { wrapper: Wrapper, ...renderOptions }) }
+function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+  const store = setupStore();
+  return <Provider store={store}>{children}</Provider>;
 }
 
 describe('useGetUsersQuery', () => {
+  // INFO [refference]: [Testing RTK Query with Jest](https://medium.com/@johnmcdowell0801/testing-rtk-query-with-jest-cdfa5aaf3dc1)
   test('returns users', async () => {
-  
-    const { result } = renderWithProviders(() => useGetUsersQuery())
-    expect(result.current.data).toBeUndefined();
+    fetchMock.mockResponse(JSON.stringify({}));
+    const { result, waitForNextUpdate } = renderHook(() => useGetUsersQuery(), {
+      wrapper: Wrapper,
+    });
+    const initialResponse = result.current;
+    expect(initialResponse.data).toBeUndefined();
+    expect(initialResponse.isLoading).toBe(true);
+
+    await act(() => waitForNextUpdate());
+
+    const nextResponse = result.current;
+    expect(nextResponse.data).not.toBeUndefined();
+    expect(nextResponse.isLoading).toBe(false);
+    expect(nextResponse.isSuccess).toBe(true);
   });
 });
