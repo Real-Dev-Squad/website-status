a6fe8458bf8175a16257dbd540c380a68f0d3762
ruthvik <ruthv1k@proton.me>
2023-03-24 16:29:51 +0530

    Handle absolute imports - slices

--- jest.config.js
+++ jest.config.js
@@ -1,36 +1,39 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const compileOptions = require('./tsconfig.json');
 module.exports = {
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
   ],
   moduleNameMapper: {
     /* Handle CSS imports (with CSS modules)
     https://jestjs.io/docs/webpack#mocking-css-modules */
-    "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
 
     // Handle CSS imports (without CSS modules)
-    "^.+\\.(css|sass|scss)$": "<rootDir>/__mocks__/styleMock.js",
+    '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
 
     /* Handle image imports
     https://jestjs.io/docs/webpack#handling-static-assets */
-    "^.+\\.(jpg|jpeg|png|gif|webp|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
 
     // Handle absolute imports
-    "^@/(.*)$": "<rootDir>/src/$1",
+    '^@/slices(.*)$': '<rootDir>/slices/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  testEnvironment: "jsdom",
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+  testEnvironment: 'jsdom',
   transform: {
     /* Use babel-jest to transpile tests with the next/babel preset
     https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
-    "^.+\\.tsx?$": "ts-jest",
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.tsx?$': 'ts-jest',
   },
   transformIgnorePatterns: [
-    "/node_modules/",
-    "^.+\\.module\\.(css|sass|scss)$",
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
   ],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
 };
