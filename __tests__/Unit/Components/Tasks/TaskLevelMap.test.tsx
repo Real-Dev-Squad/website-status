a57497cc2d88faefe9be78903b6715c4d45defea
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-04-12 15:35:28 +0530

    remove commented code

--- __tests__/Unit/Components/Tasks/TaskLevelMap.test.tsx
+++ __tests__/Unit/Components/Tasks/TaskLevelMap.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
 import { TaskLevelMap } from '@/components/tasks/card/TaskLevelMap';
 import { isUserAuthorizedContext } from '@/context/isUserAuthorized';
 describe('TaskLevelMap', () => {
