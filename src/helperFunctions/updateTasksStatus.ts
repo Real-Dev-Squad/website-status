18c8ae0ea75b1b1c6f48124916829771a2ee0a0b
Ujjawal Kumar <79056054+endeavourmonk@users.noreply.github.com>
2023-04-15 20:25:42 +0530

    Update updateTasksStatus.ts
    refactored code: removed if else ladders and usesd mapping.

--- src/helperFunctions/updateTasksStatus.ts
+++ src/helperFunctions/updateTasksStatus.ts
@@ -2,18 +2,19 @@ import task from '../interfaces/task.type';
 
 const updateTasksStatus = (tasks: Array<task>) => {
     const taskList: Array<task> = [];
-    const status = {
-        active: 'IN_PROGRESS',
-        pending: 'IN_PROGRESS',
-        assigned: 'ASSIGNED',
-        unassigned: 'AVAILABLE',
-        completed: 'COMPLETED',
-        blocked: 'BLOCKED',
-    };
+    enum Status {
+        active = 'IN_PROGRESS',
+        pending = 'IN_PROGRESS',
+        assigned = 'ASSIGNED',
+        unassigned = 'AVAILABLE',
+        completed = 'COMPLETED',
+        blocked = 'BLOCKED',
+    }
 
     tasks.forEach((taskItem, index) => {
         taskList.push(taskItem);
-        taskList[index].status = status[taskList[index].status as keyof typeof status];
+        taskList[index].status =
+            Status[taskList[index].status as keyof typeof Status];
     });
     return tasks;
 };
