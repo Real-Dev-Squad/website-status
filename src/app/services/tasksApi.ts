757c67eed7f2019b93db4751a49e05ab7193d593
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-04-15 12:25:54 +0530

    write tasks API

---
+++ src/app/services/tasksApi.ts
@@ -0,0 +1,30 @@
+import task from '@/interfaces/task.type';
+import { api } from './api';
+
+type TasksQueryResponse = {
+    message: string;
+    tasks: task[];
+};
+
+export const tasksApi = api.injectEndpoints({
+    endpoints: (builder) => ({
+        getAllTasks: builder.query<TasksQueryResponse, unknown>({
+            query: () => '/tasks',
+            providesTags: ['Tasks'],
+            transformResponse: (response: TasksQueryResponse) => {
+                return {
+                    ...response,
+                    tasks: response.tasks.sort(
+                        (a: task, b: task) => +a.endsOn - +b.endsOn
+                    ),
+                };
+            },
+        }),
+        getMineTasks: builder.query<TasksQueryResponse, unknown>({
+            query: () => '/tasks/mine',
+            providesTags: ['Tasks'],
+        }),
+    }),
+});
+
+export const { useGetAllTasksQuery, useGetMineTasksQuery } = tasksApi;
