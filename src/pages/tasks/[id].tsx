886821a3a3a38ccb3f26d5f0c575d953898a0b0c
kotesh-arya <koteswarraomudila@gmail.com>
2023-04-18 00:49:03 +0530

    REFACTOR: remove unnecessary FC type

--- src/pages/tasks/[id].tsx
+++ src/pages/tasks/[id].tsx
@@ -1,17 +1,16 @@
-import { FC } from 'react';
-import { NextRouter, useRouter } from 'next/router';
+import { useRouter } from 'next/router';
 import TaskDetails from '@/components/taskDetails';
 import { TASKS_URL } from '@/components/constants/url';
 
-const TaskDetailsPage: FC = () => {
-  const router: NextRouter = useRouter();
-  const id: string = router.query.id as string;
-  const TASK_DETAILS_URL = `${TASKS_URL}/${id}/details`;
+const TaskDetailsPage = () => {
+    const router = useRouter();
+    const id = router.query?.id as string;
+    const TASK_DETAILS_URL = `${TASKS_URL}/${id}/details`;
 
-  if (!id) {
-    return null;
-  }
+    if (!id) {
+        return null;
+    }
 
-  return <TaskDetails url={TASK_DETAILS_URL} taskID={id} />;
+    return <TaskDetails url={TASK_DETAILS_URL} taskID={id} />;
 };
 export default TaskDetailsPage;
