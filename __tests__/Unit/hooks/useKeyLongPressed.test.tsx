bfa78855ff07d9255986c146a46add080d5aebc4
Bhavika Tibrewal <bhtibrewal@gmail.com>
2023-04-18 23:16:26 +0530

    remove formatting changes

--- __tests__/Unit/hooks/useKeyLongPressed.test.tsx
+++ __tests__/Unit/hooks/useKeyLongPressed.test.tsx
@@ -1,59 +1,59 @@
-import { act, renderHook } from '@testing-library/react-hooks';	
-import { useKeyLongPressed } from '../../../src/hooks/useKeyLongPressed';	
+import { act, renderHook } from '@testing-library/react-hooks';
+import { useKeyLongPressed } from '../../../src/hooks/useKeyLongPressed';
 
-jest.useFakeTimers();	
-describe('useKeyLongPressed', () => {	
-    it('should return an empty string when no keys are pressed', () => {	
-        const { result } = renderHook(() => useKeyLongPressed());	
+jest.useFakeTimers();
+describe('useKeyLongPressed', () => {
+    it('should return an empty string when no keys are pressed', () => {
+        const { result } = renderHook(() => useKeyLongPressed());
 
-        expect(result.current[0]).toBe('');	
-    });	
+        expect(result.current[0]).toBe('');
+    });
 
-    it('should set the keyLongPressed state when a key is held down for 300ms', () => {	
-        const { result } = renderHook(() => useKeyLongPressed());	
+    it('should set the keyLongPressed state when a key is held down for 300ms', () => {
+        const { result } = renderHook(() => useKeyLongPressed());
 
-        act(() => {	
-            const event = new KeyboardEvent('keydown', { key: 'a' });	
-            document.dispatchEvent(event);	
-            jest.advanceTimersByTime(300);	
-        });	
+        act(() => {
+            const event = new KeyboardEvent('keydown', { key: 'a' });
+            document.dispatchEvent(event);
+            jest.advanceTimersByTime(300);
+        });
 
-        expect(result.current[0]).toBe('a');	
-    });	
+        expect(result.current[0]).toBe('a');
+    });
 
-    it('should clear the keyLongPressed state when the key is released before 300ms', () => {	
-        const { result } = renderHook(() => useKeyLongPressed());	
+    it('should clear the keyLongPressed state when the key is released before 300ms', () => {
+        const { result } = renderHook(() => useKeyLongPressed());
 
-        act(() => {	
-            const event = new KeyboardEvent('keydown', { key: 'a' });	
-            document.dispatchEvent(event);	
-            jest.advanceTimersByTime(100);	
-            const upEvent = new KeyboardEvent('keyup');	
-            document.dispatchEvent(upEvent);	
-        });	
+        act(() => {
+            const event = new KeyboardEvent('keydown', { key: 'a' });
+            document.dispatchEvent(event);
+            jest.advanceTimersByTime(100);
+            const upEvent = new KeyboardEvent('keyup');
+            document.dispatchEvent(upEvent);
+        });
 
-        expect(result.current[0]).toBe('');	
-    });	
+        expect(result.current[0]).toBe('');
+    });
 
-    it('should remove event listeners when the component unmounts', () => {	
-        const removeEventListenerSpy = jest.spyOn(	
-            document,	
-            'removeEventListener'	
-        );	
+    it('should remove event listeners when the component unmounts', () => {
+        const removeEventListenerSpy = jest.spyOn(
+            document,
+            'removeEventListener'
+        );
 
-        const { unmount } = renderHook(() => useKeyLongPressed());	
-        unmount();	
+        const { unmount } = renderHook(() => useKeyLongPressed());
+        unmount();
 
-        expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);	
-        expect(removeEventListenerSpy).toHaveBeenCalledWith(	
-            'keydown',	
-            expect.any(Function)	
-        );	
-        expect(removeEventListenerSpy).toHaveBeenCalledWith(	
-            'keyup',	
-            expect.any(Function)	
-        );	
+        expect(removeEventListenerSpy).toHaveBeenCalledTimes(2);
+        expect(removeEventListenerSpy).toHaveBeenCalledWith(
+            'keydown',
+            expect.any(Function)
+        );
+        expect(removeEventListenerSpy).toHaveBeenCalledWith(
+            'keyup',
+            expect.any(Function)
+        );
 
-        removeEventListenerSpy.mockRestore();	
-    });	
+        removeEventListenerSpy.mockRestore();
+    });
 });
