a125d0e2613192422775809907a8274afcaad6cd
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-04-23 11:25:46 +0530

    fixed responsiveness of navbar and tested

---
+++ src/pages/task/[id].js
@@ -0,0 +1,20 @@
+import { useRouter } from 'next/router';
+
+const TaskDetailsPage = () => {
+  
+  const router = useRouter();
+  const {title,type,status,links,participants,assignee,startedOn,endsOn} = router.query;
+    
+  return(
+  <>
+  {/* dummy UI */}
+  <div>This is tasks details page</div>
+  <p>
+    Title:{title},Type:{type},status:{status},links:{links},participants:{participants},
+    assignee:{assignee},startedOn:{startedOn},endsOn:{endsOn}
+    </p>
+  </>
+  );
+};
+
+export default TaskDetailsPage;
