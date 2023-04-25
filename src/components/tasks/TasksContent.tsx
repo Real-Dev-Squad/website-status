9dfd11a53b4d1d9bb401dc6786f8c62eb4d4e928
Bharati Subramanian <bharatisharada@gmail.com>
2023-04-25 17:48:40 +0530

    refactor: revert Tasks Content to changes prev RTK query

--- src/components/tasks/TasksContent.tsx
+++ src/components/tasks/TasksContent.tsx
@@ -1,29 +1,94 @@
 import classNames from '@/styles/tasks.module.scss';
-import { useGetAllTasksQuery } from '@/app/services/tasksApi';
+// import { useGetAllTasksQuery } from '@/app/services/tasksApi';
 import { useAppContext } from '@/context';
 import { isUserAuthorizedContext } from '@/context/isUserAuthorized';
 import updateTasksStatus from '@/helperFunctions/updateTasksStatus';
 import task, { Tab } from '@/interfaces/task.type';
-import { useContext, useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
+import {
+    ASSIGNED,
+    COMPLETED,
+    AVAILABLE,
+    IN_PROGRESS,
+    SMOKE_TESTING,
+    NEEDS_REVIEW,
+    IN_REVIEW,
+    APPROVED,
+    MERGED,
+    SANITY_CHECK,
+    REGRESSION_CHECK,
+    RELEASED,
+    VERIFIED,
+    BLOCKED,
+} from '@/components/constants/task-status';
 import {
     NO_TASKS_FOUND_MESSAGE,
     TASKS_FETCH_ERROR_MESSAGE,
 } from '../constants/messages';
+import { TASKS_URL } from '@/components/constants/url';
 import { TabSection } from './TabSection';
 import TaskList from './TaskList/TaskList';
 import updateCardContent from '@/helperFunctions/updateCardContent';
+import useFetch from '@/hooks/useFetch';
+import useUpdateTask from '@/hooks/useUpdateTask';
+import groupTasksByStatus from '@/utils/groupTasksByStatus';
+
+const STATUS_ORDER = [
+    ASSIGNED,
+    COMPLETED,
+    BLOCKED,
+    AVAILABLE,
+    IN_PROGRESS,
+    SMOKE_TESTING,
+    NEEDS_REVIEW,
+    IN_REVIEW,
+    APPROVED,
+    MERGED,
+    SANITY_CHECK,
+    REGRESSION_CHECK,
+    RELEASED,
+    VERIFIED,
+];
 
 export const TasksContent = () => {
     const { state: appState } = useAppContext();
-    const { data: tasks = [], isError, isLoading } = useGetAllTasksQuery();
+    // TODO: the below code should be used when mutation for updating tasks is implemented
+    // const { data: tasks = [], isError, isLoading } = useGetAllTasksQuery();
     const { isEditMode } = appState;
     const isUserAuthorized = useContext(isUserAuthorizedContext);
     const isEditable = isUserAuthorized && isEditMode;
     const [activeTab, setActiveTab] = useState(Tab.ASSIGNED);
+    // TODO: the below code should removed when mutation for updating tasks is implemented
+    const [filteredTask, setFilteredTask] = useState<any>([]);
+    // TODO: the below code should removed when mutation for updating tasks is implemented
+    const { response, isLoading: loading, error } = useFetch(TASKS_URL);
+    // TODO: the below code should removed when mutation for updating tasks is implemented
+    const updateTask = useUpdateTask(filteredTask, setFilteredTask);
 
     const onSelect = (tab: Tab) => {
         setActiveTab(tab);
     };
+
+    // TODO: the useEffect should be removed when mutation for updating tasks is implemented
+    useEffect(() => {
+        if ('tasks' in response) {
+            const tasks = updateTasksStatus(response.tasks);
+            tasks.sort((a: task, b: task) => +a.endsOn - +b.endsOn);
+            tasks.sort(
+                (a: task, b: task) =>
+                    STATUS_ORDER.indexOf(a.status) -
+                    STATUS_ORDER.indexOf(b.status)
+            );
+            const taskMap: any = groupTasksByStatus(tasks);
+            setFilteredTask(taskMap);
+        }
+        return () => {
+            setFilteredTask([]);
+        };
+    }, [loading, response]);
+
+    // TODO: the below code should be used when mutation for updating tasks is implemented
+    /*
     const tasksGroupedByStatus = updateTasksStatus(tasks).reduce(
         (acc: Record<string, task[]>, curr: task) => {
             return acc[curr.status as keyof task]
@@ -35,20 +100,22 @@ export const TasksContent = () => {
         },
         {}
     );
+    */
 
-    if (isError) return <p>{TASKS_FETCH_ERROR_MESSAGE}</p>;
+    if (error) return <p>{TASKS_FETCH_ERROR_MESSAGE}</p>;
 
-    if (isLoading) return <p>Loading...</p>;
+    if (loading) return <p>Loading...</p>;
 
     return (
         <div className={classNames.tasksContainer}>
             <TabSection onSelect={onSelect} activeTab={activeTab} />
             <div>
-                {tasksGroupedByStatus[activeTab] ? (
+                {filteredTask[activeTab] ? (
                     <TaskList
-                        tasks={tasksGroupedByStatus[activeTab]}
+                        tasks={filteredTask[activeTab]}
                         isEditable={isEditable}
                         updateCardContent={updateCardContent}
+                        updateTask={updateTask}
                     />
                 ) : (
                     <p>{NO_TASKS_FOUND_MESSAGE}</p>
