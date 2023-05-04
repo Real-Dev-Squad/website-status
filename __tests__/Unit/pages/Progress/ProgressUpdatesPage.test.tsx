b308c1a4e91783fb3f63a23d8c4919dc1a59080f
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-05-04 14:15:36 +0530

    add tests for progres updates page component

---
+++ __tests__/Unit/pages/Progress/ProgressUpdatesPage.test.tsx
@@ -0,0 +1,45 @@
+import { store } from '@/app/store';
+import ProgressUpdatesPage from '@/pages/progress/[id]';
+import { renderWithRouter } from '@/test_utils/createMockRouter';
+import { screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+
+describe('Progress page test', function () {
+    it('Renders a form', function () {
+        renderWithRouter(
+            <Provider store={store()}>
+                <ProgressUpdatesPage />
+            </Provider>,
+            { query: { id: '123', dev: 'true' } }
+        );
+        const textBoxes = screen.getAllByRole('textbox');
+        expect(textBoxes).toHaveLength(3);
+        const button = screen.getByRole('button') as HTMLButtonElement;
+        expect(button).toBeInTheDocument();
+        expect(button.type).toBe('submit');
+    });
+
+    it('should return 404 if no id is passed', function (){
+        renderWithRouter(
+            <Provider store={store()}>
+                <ProgressUpdatesPage />
+            </Provider>,
+            { query: { dev: 'true' } }
+        );
+        const headings = screen.getAllByRole('heading');
+        expect(headings).toHaveLength(1);
+        expect(headings[0].innerHTML).toBe('404 - Page Not Found');
+    });
+
+    it('should return 404 if no dev flag is passed', function (){
+        renderWithRouter(
+            <Provider store={store()}>
+                <ProgressUpdatesPage />
+            </Provider>,
+            { query: { id: '123' } }
+        );
+        const headings = screen.getAllByRole('heading');
+        expect(headings).toHaveLength(1);
+        expect(headings[0].innerHTML).toBe('404 - Page Not Found');
+    });
+});
