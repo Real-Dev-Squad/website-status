9ea02089bc642030e985db972964f119580ef072
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-05-06 16:23:02 +0530

    add variables for colours and  remove unwanted fragment

--- src/components/ProgressForm/InputWithQuestions.tsx
+++ src/components/ProgressForm/InputWithQuestions.tsx
@@ -5,23 +5,17 @@ import { inputPropsTypes } from '@/types/ProgressUpdates';
 const InputWithQuestions = ({ name, question }: inputPropsTypes) => {
     const [answer, setAnswer] = useState('');
     return (
-        <>
-            <div className={styles.inputComponent}>
-                <label
-                    className={styles.label}
-                    htmlFor={name}
-                    aria-label={name}
-                >
-                    {question}
-                </label>
-                <textarea
-                    className={styles.input}
-                    value={answer}
-                    onChange={(e) => setAnswer(e.target.value)}
-                    id={name}
-                />
-            </div>
-        </>
+        <div className={styles.inputComponent}>
+            <label className={styles.label} htmlFor={name} aria-label={name}>
+                {question}
+            </label>
+            <textarea
+                className={styles.input}
+                value={answer}
+                onChange={(e) => setAnswer(e.target.value)}
+                id={name}
+            />
+        </div>
     );
 };
 
