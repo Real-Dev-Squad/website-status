c1b9a6e4590482d9b37f3481940de7f9822caed1
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-05-06 08:19:12 +0530

    Fix prettier issue in taskDetails test

--- __tests__/Unit/Components/Tasks/TaskDetails.test.tsx
+++ __tests__/Unit/Components/Tasks/TaskDetails.test.tsx
@@ -14,11 +14,13 @@ describe('TaskDetails Page', () => {
     });
     test('Task title is Editable in Editing mode ', () => {
         render(<TaskDetails url={details.url} taskID={details.taskID} />);
+
         const titleElement = screen.queryByTestId('task-title');
         expect(titleElement).not.toBeInTheDocument();
     });
     test('Edit button is not rendered when Editing', () => {
         render(<TaskDetails url={details.url} taskID={details.taskID} />);
+
         const editButtonElement = screen.queryByRole('button', {
             name: 'Edit',
         });
@@ -26,6 +28,7 @@ describe('TaskDetails Page', () => {
     });
     test('Task Description is Editable in Editing mode', () => {
         render(<TaskDetails url={details.url} taskID={details.taskID} />);
+
         const descriptionElement = screen.queryByText(
             /No description available/i
         );
