2ddfbb5db5537eecfc1596e19f4a89c0a06693f8
Ritik Jaiswal <ritik12103032000@gmail.com>
2023-05-06 08:17:50 +0530

    Remove task details test changes

--- __tests__/Unit/Components/Tasks/TaskDetails.test.tsx
+++ __tests__/Unit/Components/Tasks/TaskDetails.test.tsx
@@ -1,8 +1,5 @@
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TaskDetails from '@/components/taskDetails';
-import { renderWithRouter } from '@/test_utils/createMockRouter';
-import { Provider } from 'react-redux';
-import { store } from '@/app/store';
 
 const details = {
     url: 'https://realdevsquad.com/tasks/6KhcLU3yr45dzjQIVm0J/details',
@@ -11,43 +8,24 @@ const details = {
 
 describe('TaskDetails Page', () => {
     test('Loading text rendered when loading', () => {
-        renderWithRouter(
-            <Provider store={store()}>
-                <TaskDetails url={details.url} taskID={details.taskID} />
-            </Provider>
-        );
+        render(<TaskDetails url={details.url} taskID={details.taskID} />);
         const loadingElement = screen.getByText(/Loading.../i);
         expect(loadingElement).toBeInTheDocument();
     });
     test('Task title is Editable in Editing mode ', () => {
-        renderWithRouter(
-            <Provider store={store()}>
-                <TaskDetails url={details.url} taskID={details.taskID} />
-            </Provider>
-        );
-
+        render(<TaskDetails url={details.url} taskID={details.taskID} />);
         const titleElement = screen.queryByTestId('task-title');
         expect(titleElement).not.toBeInTheDocument();
     });
     test('Edit button is not rendered when Editing', () => {
-        renderWithRouter(
-            <Provider store={store()}>
-                <TaskDetails url={details.url} taskID={details.taskID} />
-            </Provider>
-        );
-
+        render(<TaskDetails url={details.url} taskID={details.taskID} />);
         const editButtonElement = screen.queryByRole('button', {
             name: 'Edit',
         });
         expect(editButtonElement).not.toBeInTheDocument();
     });
     test('Task Description is Editable in Editing mode', () => {
-        renderWithRouter(
-            <Provider store={store()}>
-                <TaskDetails url={details.url} taskID={details.taskID} />
-            </Provider>
-        );
-
+        render(<TaskDetails url={details.url} taskID={details.taskID} />);
         const descriptionElement = screen.queryByText(
             /No description available/i
         );
