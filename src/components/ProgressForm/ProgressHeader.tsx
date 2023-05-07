6aa3ac787b7e1eec2247bd2c91318d9a0e5c41b5
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-05-08 00:55:54 +0530

    fix inconsistent spacing

--- src/components/ProgressForm/ProgressHeader.tsx
+++ src/components/ProgressForm/ProgressHeader.tsx
@@ -4,8 +4,8 @@ function ProgressHeader() {
     return (
         <header className={styles.header}>
             <p>
-                You have <span className={styles.mark}> 2 missed </span>
-                Progress Updates
+                You have <span className={styles.mark}> 2 missed</span> Progress
+                Updates
             </p>
             <p>Lets try to avoid missing updates</p>
         </header>
