a54b480ad88c1df563bf272c07c5dd65f26bc035
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-05-11 17:54:28 +0530

    add tests for standup dashboard

---
+++ __tests__/Unit/Components/Searchbar/Searchbar.test.tsx
@@ -0,0 +1,60 @@
+import Searchbar from '@/components/Dashboard/Searchbar';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+let mockFn: jest.Func;
+
+beforeEach(() => {
+    mockFn = jest.fn();
+});
+
+describe('test searchbar component', function () {
+    it('renders the searchbar with appropriate label', function () {
+        render(<Searchbar label="test-label" handleSearch={mockFn} />);
+
+        const label = screen.getByLabelText('test-label');
+        expect(label).toBeInTheDocument();
+    });
+
+    it('checks if we can type into the searchbar', function () {
+        render(<Searchbar label="test-label" handleSearch={mockFn} />);
+
+        const input = screen.getByLabelText('test-label') as HTMLInputElement;
+
+        fireEvent.click(input);
+        fireEvent.change(input, { target: { value: '123,456' } });
+        expect(input.value).toBe('123,456');
+    });
+
+    it('tests if the click handler is called', function () {
+        render(<Searchbar label="test-label" handleSearch={mockFn} />);
+
+        const input = screen.getByLabelText('test-label') as HTMLInputElement;
+
+        const searchButton = screen.getByRole('button');
+
+        fireEvent.change(input, { tatget: { value: '123' } });
+        fireEvent.click(searchButton);
+
+        expect(mockFn).toBeCalledTimes(1);
+    });
+
+    it('tests if enter key calls search', function () {
+        render(<Searchbar label="test-label" handleSearch={mockFn} />);
+
+        const input = screen.getByLabelText('test-label') as HTMLInputElement;
+
+        fireEvent.keyDown(input, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+        expect(mockFn).toBeCalledTimes(1);
+    });
+
+    it('tests other key down events do not call search', function () {
+        render(<Searchbar label="test-label" handleSearch={mockFn} />);
+
+        const input = screen.getByLabelText('test-label') as HTMLInputElement;
+
+        fireEvent.keyDown(input, { key: 'A', code: 'KeyA' });
+
+        expect(mockFn).toBeCalledTimes(0);
+    });
+});
---
+++ __tests__/Unit/pages/Dashboard/dashboardPage.test.tsx
@@ -0,0 +1,49 @@
+import DashboardPage from '@/pages/dashboard';
+import { renderWithRouter } from '@/test_utils/createMockRouter';
+import { Provider } from 'react-redux';
+import { store } from '@/app/store';
+import { screen } from '@testing-library/react';
+import { setupServer } from 'msw/node';
+import handlers from '../../../../__mocks__/handlers';
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+    server.listen();
+});
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
+describe('dashboard page test', function () {
+    it('checks if the page is rendered with exact components', function () {
+        renderWithRouter(
+            <Provider store={store()}>
+                <DashboardPage />
+            </Provider>,
+            { query: { dev: 'true' } }
+        );
+
+        const searchBar = screen.getByRole('textbox');
+        expect(searchBar).toBeInTheDocument();
+
+        const searchButton = screen.getByRole('button');
+        expect(searchButton.innerHTML).toBe('Search');
+
+        const label = screen.getByLabelText('Users');
+        expect(label).toBeInTheDocument();
+    });
+
+    it('renders 404 without passing the feature flag', function () {
+        renderWithRouter(
+            <Provider store={store()}>
+                <DashboardPage />
+            </Provider>
+        );
+
+        const headings = screen.getAllByRole('heading');
+        expect(headings).toHaveLength(1);
+        expect(headings[0].innerHTML).toBe('404 - Page Not Found');
+    });
+});
