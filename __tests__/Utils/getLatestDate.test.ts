9d908912fe692adbaf27b6bf0a5df67a7155f073
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-05-12 09:38:31 +0530

    add tests for getLatestDate utility

---
+++ __tests__/Utils/getLatestDate.test.ts
@@ -0,0 +1,11 @@
+import getCurrentDate from '@/utils/getLatestDate';
+
+describe('test getLatest Date', function () {
+    it('returns the latest date in string format', function () {
+        jest.useFakeTimers();
+        jest.setSystemTime(1683863962266);
+        const currentDate = getCurrentDate();
+
+        expect(currentDate).toBe('Friday, 12 May 2023');
+    });
+});
