b1829c6fe39fff2579ee9118f750df5d6f64d837
Ritik Jaiswal <57758447+RitikJaiswal75@users.noreply.github.com>
2023-05-20 04:48:01 +0530

    Revert "Add a react-markdown library to convert the Markdown code to HTML format"

--- src/components/issues/Card.tsx
+++ src/components/issues/Card.tsx
@@ -1,5 +1,4 @@
 import { FC, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
 import styles from '@/components/issues/Card.module.scss';
 import { toast, ToastTypes } from '@/helperFunctions/toast';
 
@@ -62,8 +61,44 @@ const Card: FC<IssueCardProps> = ({ issue }) => {
 
     return (
         <div className={styles.card}>
-            <div className={styles.card__top__details__button}>
-                <h2 className={styles.card__title}>{issue.title}</h2>
+            <div className={styles.card__top}>
+                <div className={styles.card__top__details}>
+                    <div className={styles.card__top__details__meta_data}>
+                        <h2 className={styles.card__title}>{issue.title}</h2>
+                        <p>
+                            Opened on {created} by {issue.user.login}
+                            <br></br>
+                        </p>
+                        <p className="card__body">{issue.body}</p>
+                        {issue.assignee
+                            ? 'Assigned to ' + issue.assignee.login
+                            : ''}
+                        <p className={styles.card__link}>
+                            Issue Link :{' '}
+                            <a
+                                href={issue.html_url}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                {issue.html_url}
+                            </a>{' '}
+                        </p>
+                    </div>
+
+                    <div className={styles.label__block}>
+                        {issue.labels.map(
+                            (label: {
+                                id: number | null | undefined;
+                                name: string | null | undefined;
+                            }) => (
+                                <button className={styles.label} key={label.id}>
+                                    {label.name}
+                                </button>
+                            )
+                        )}
+                    </div>
+                </div>
+
                 <button
                     className={styles.card__top__button}
                     disabled={taskExists || isLoading}
@@ -71,34 +106,6 @@ const Card: FC<IssueCardProps> = ({ issue }) => {
                 >
                     Convert to task
                 </button>
-            </div>
-            <p className={styles.card__issue_created__by}>
-                Opened on {created} by {issue.user.login}
-            </p>
-            <div className="card__body">
-                <ReactMarkdown className={styles.card__body__description}>
-                    {issue.body ?? 'No description provided'}
-                </ReactMarkdown>
-            </div>
-            {issue.assignee && 'Assigned to ' + issue.assignee.login}
-            <p className={styles.card__link}>
-                Issue Link :{' '}
-                <a href={issue.html_url} target="_blank" rel="noreferrer">
-                    {issue.html_url}
-                </a>{' '}
-            </p>
-
-            <div className={styles.label__block}>
-                {issue.labels.map(
-                    (label: {
-                        id: number | null | undefined;
-                        name: string | null | undefined;
-                    }) => (
-                        <button className={styles.label} key={label.id}>
-                            {label.name}
-                        </button>
-                    )
-                )}
             </div>
         </div>
     );
