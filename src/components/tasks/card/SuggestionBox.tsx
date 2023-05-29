b2e01739cb0a94cbb385334e414e55643e1a6aeb
FMK2312 <fmkhandwala39@gmail.com>
2023-05-29 11:03:01 +0530

    refactored the component and inroduced a try catch in fetch

--- src/components/tasks/card/SuggestionBox.tsx
+++ src/components/tasks/card/SuggestionBox.tsx
@@ -4,20 +4,21 @@ import classNames from '@/components/tasks/card/suggestion.module.scss';
 import fetch from '@/helperFunctions/fetch';
 import { userDataType } from '@/interfaces/user.type';
 import { GithubInfo } from '@/interfaces/suggestionBox.type';
+import { toast, ToastTypes } from '@/helperFunctions/toast';
 
 type Props = {
     assigneeName: string;
-    showSuggestion: boolean;
     onClickName: (userName: string) => void;
 };
 
 const SuggestionBox: FC<Props> = ({
     assigneeName,
-    showSuggestion = false,
     onClickName = () => undefined,
 }) => {
     const [loading, setLoading] = useState<boolean>(false);
     const [suggestions, setSuggestions] = useState<GithubInfo[]>([]);
+
+    const { ERROR } = ToastTypes;
 
     const clickHandler = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
         onClickName(e.currentTarget.innerText);
@@ -26,21 +27,29 @@ const SuggestionBox: FC<Props> = ({
     const fetchUsers = async () => {
         setLoading(true);
         const url = `${process.env.NEXT_PUBLIC_BASE_URL}/users?search=${assigneeName}`;
-        const { requestPromise } = fetch({ url });
-        const users = await requestPromise;
-        const usersData = users.data.users;
-        const suggestedUsers: GithubInfo[] = [];
-        usersData.map((data: userDataType) => {
-            suggestedUsers.push({
-                github_id: data.github_id,
-                profileImageUrl: data?.picture?.url
-                    ? data.picture.url
-                    : placeholderImageURL,
+        try {
+            const { requestPromise } = fetch({ url });
+            const users = await requestPromise;
+            const usersData = users.data.users;
+            const suggestedUsers: GithubInfo[] = [];
+            usersData.map((data: userDataType) => {
+                suggestedUsers.push({
+                    github_id: data.github_id,
+                    profileImageUrl: data?.picture?.url
+                        ? data.picture.url
+                        : placeholderImageURL,
+                });
             });
-        });
 
-        setSuggestions(suggestedUsers);
-        setLoading(false);
+            setSuggestions(suggestedUsers);
+            setLoading(false);
+        } catch (error: any) {
+            if ('response' in error) {
+                toast(ERROR, error.response.data.message);
+                return;
+            }
+            toast(ERROR, error.message);
+        }
     };
 
     useEffect(() => {
@@ -58,46 +67,97 @@ const SuggestionBox: FC<Props> = ({
         };
     }, [assigneeName]);
 
-    let suggestionList;
+    // let suggestionList;
 
-    if (showSuggestion && assigneeName) {
-        if (suggestions.length) {
-            suggestionList = (
-                <ul className={classNames['suggestions']}>
-                    {suggestions.map((suggestion: GithubInfo) => {
-                        return (
-                            <li
-                                key={suggestion.github_id}
-                                onClick={clickHandler}
-                            >
-                                <span>{suggestion.github_id}</span>
-                                <img
-                                    src={suggestion.profileImageUrl}
-                                    width={20}
-                                    height={20}
-                                />
-                            </li>
-                        );
-                    })}
-                </ul>
-            );
-        } else {
-            suggestionList = (
-                <span className={classNames['no-suggestions']}>
-                    User not found!
-                </span>
-            );
-        }
-    }
+    // if (assigneeName) {
+    //     if (suggestions.length) {
+    //         suggestionList = (
+    //             <ul className={classNames['suggestions']}>
+    //                 {suggestions.map((suggestion: GithubInfo) => {
+    //                     return (
+    //                         <li
+    //                             key={suggestion.github_id}
+    //                             onClick={clickHandler}
+    //                         >
+    //                             <span>{suggestion.github_id}</span>
+    //                             <img
+    //                                 src={suggestion.profileImageUrl}
+    //                                 width={20}
+    //                                 height={20}
+    //                             />
+    //                         </li>
+    //                     );
+    //                 })}
+    //             </ul>
+    //         );
+    //     } else {
+    //         suggestionList = (
+    //             <span className={classNames['no-suggestions']}>
+    //                 User not found!
+    //             </span>
+    //         );
+    //     }
+    // }
 
-    return (
-        <div style={{ position: 'relative' }}>
-            {loading && (
-                <span className={classNames['loading']}>Loading..</span>
-            )}
-            {!loading && suggestionList}
-        </div>
-    );
+    // return (
+    //     <div style={{ position: 'relative' }}>
+    //         {loading && (
+    //             <span className={classNames['loading']}>Loading..</span>
+    //         )}
+    //         {!loading && suggestionList}
+    //     </div>
+    // );
+
+    const SuggestionList = () => {
+        return (
+            <ul className={classNames['suggestions']}>
+                {suggestions.map((suggestion: GithubInfo) => {
+                    return (
+                        <li key={suggestion.github_id} onClick={clickHandler}>
+                            <span>{suggestion.github_id}</span>
+                            <img
+                                src={suggestion.profileImageUrl}
+                                width={20}
+                                height={20}
+                            />
+                        </li>
+                    );
+                })}
+            </ul>
+        );
+    };
+
+    const Loading = () => {
+        return <span className={classNames['loading']}>Loading...</span>;
+    };
+
+    const UserNotFound = () => {
+        return (
+            <span className={classNames['no-suggestions']}>
+                User not found!
+            </span>
+        );
+    };
+
+    if (loading)
+        return (
+            <div style={{ position: 'relative' }}>
+                <Loading />
+            </div>
+        );
+    if (suggestions.length === 0 && assigneeName.trim() !== '')
+        return (
+            <div style={{ position: 'relative' }}>
+                <UserNotFound />
+            </div>
+        );
+    if (suggestions.length)
+        return (
+            <div style={{ position: 'relative' }}>
+                <SuggestionList />
+            </div>
+        );
+    return <></>;
 };
 
 export default SuggestionBox;
