a9f35418985703802d41e64c37da5559b1c70d20
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-05-31 16:15:30 +0530

    fixed edge case of total missed update

--- src/utils/getTotalMissedUpdate.ts
+++ src/utils/getTotalMissedUpdate.ts
@@ -1,12 +1,19 @@
 export function getTotalMissedUpdate(date: Array<number>): number {
     const currentStandupDate = new Date().setUTCHours(0, 0, 0, 0);
     let lastStanupDate = 0;
+    let totalMissedUpdate = 0;
+    if (date.length === 0) {
+        return totalMissedUpdate;
+    }
     for (let i = 0; i < date.length; i++) {
         if (date[i] > lastStanupDate) {
             lastStanupDate = date[i];
         }
     }
-    const totalMissedUpdate =
+    console.log(currentStandupDate);
+    totalMissedUpdate =
         (currentStandupDate - lastStanupDate) / (24 * 60 * 60 * 1000);
+    console.log(totalMissedUpdate);
     return totalMissedUpdate;
 }
+//86,400,000
