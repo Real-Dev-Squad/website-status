5e316b4971bba245128369769267b20c45b61f6a
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-06-04 20:56:29 +0530

    used moment library in getTotalMissedUpdates function

--- src/utils/getTotalMissedUpdate.ts
+++ src/utils/getTotalMissedUpdate.ts
@@ -1,16 +1,20 @@
+import moment from 'moment';
+
 export function getTotalMissedUpdates(date: Array<number>): number {
-    const currentStandupDate = new Date().setUTCHours(0, 0, 0, 0);
-    let lastStanupDate = 0;
+    const currentStandupDate = moment().format('MM DD YYYY');
+    let lastStanup = 0;
     let totalMissedUpdates = 0;
     if (date.length === 0) {
         return totalMissedUpdates;
     }
     for (let i = 0; i < date.length; i++) {
-        if (date[i] > lastStanupDate) {
-            lastStanupDate = date[i];
+        if (date[i] > lastStanup) {
+            lastStanup = date[i];
         }
     }
-    totalMissedUpdates =
-        Math.floor(currentStandupDate - lastStanupDate) / (24 * 60 * 60 * 1000);
-    return totalMissedUpdates;
+    const lastStanupDate = moment(lastStanup).format('MM DD YYYY');
+    const startDate = moment(currentStandupDate, 'MM DD YYYY');
+    const endDate = moment(lastStanupDate, 'MM DD YYYY');
+    totalMissedUpdates = startDate.diff(endDate, 'days');
+    return Number(moment(totalMissedUpdates));
 }
