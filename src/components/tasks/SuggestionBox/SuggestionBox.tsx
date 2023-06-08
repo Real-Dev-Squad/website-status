485e94b3e18141365111f42c4657e9de8278b5d8
FMK2312 <fmkhandwala39@gmail.com>
2023-06-08 23:08:22 +0530

    removed unecessary imports

--- src/components/tasks/SuggestionBox/SuggestionBox.tsx
+++ src/components/tasks/SuggestionBox/SuggestionBox.tsx
@@ -1,9 +1,5 @@
-import React, { FC, useEffect, useState } from 'react';
-// import { DUMMY_PROFILE as placeholderImageURL } from '@/constants/display-sections';
-// import fetch from '@/helperFunctions/fetch';
-// import { userDataType } from '@/interfaces/user.type';
+import React, { FC } from 'react';
 import { GithubInfo } from '@/interfaces/suggestionBox.type';
-// import { toast, ToastTypes } from '@/helperFunctions/toast';
 import SuggestionList from './SuggestionList';
 import Loading from './Loading';
 import UserNotFound from './UserNotFound';
