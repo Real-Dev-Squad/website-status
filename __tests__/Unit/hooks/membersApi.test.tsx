5a0a8158c71c552d498e5e36388683110989cf6e
Ajeyakrishna <connect.ajeya@gmail.com>
2023-06-08 07:24:34 +0530

    test case: idle members query

---
+++ __tests__/Unit/hooks/membersApi.test.tsx
@@ -0,0 +1,44 @@
+import { setupServer } from 'msw/node';
+import handlers from '../../../__mocks__/handlers/members.handler.js';
+import React, { PropsWithChildren } from 'react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { Provider } from 'react-redux';
+import { store } from '@/app/store';
+import { useGetIdleMembersQuery } from '@/app/services/membersApi';
+
+const server = setupServer(...handlers);
+
+beforeAll(() => {
+    server.listen();
+});
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+function Wrapper({
+    children,
+}: PropsWithChildren<Record<string, never>>): JSX.Element {
+    return <Provider store={store()}>{children}</Provider>;
+}
+
+describe('useGetIdleMembersQuery', () => {
+    test('returns idle members', async () => {
+        const { result, waitForNextUpdate } = renderHook(
+            () => useGetIdleMembersQuery(),
+            {
+                wrapper: Wrapper,
+            }
+        );
+
+        const initialResponse = result.current;
+        expect(initialResponse.data).toBeUndefined();
+        expect(initialResponse.isLoading).toBe(true);
+
+        await act(() => waitForNextUpdate());
+
+        const nextResponse = result.current;
+        expect(nextResponse.data).not.toBeUndefined();
+        expect(nextResponse.isLoading).toBe(false);
+        expect(nextResponse.isSuccess).toBe(true);
+        expect(nextResponse.data).toBeDefined();
+    });
+});
