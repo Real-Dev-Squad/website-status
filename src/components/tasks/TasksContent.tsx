c418feca4c8d5b2843c1d7d069d60418ee4ecc5f
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-06-09 12:13:14 +0530

    Re-Fractor the task content tab

--- src/components/tasks/TasksContent.tsx
+++ src/components/tasks/TasksContent.tsx
@@ -25,7 +25,7 @@ export const TasksContent = () => {
     const { isEditMode } = useEditMode();
     const isUserAuthorized = useContext(isUserAuthorizedContext);
     const isEditable = isUserAuthorized && isEditMode;
-    const [activeTab, setActiveTab] = useState(Tab.ASSIGNED);
+    const [activeTab, setActiveTab] = useState(Tab.IN_PROGRESS);
     // TODO: the below code should removed when mutation for updating tasks is implemented
     const [filteredTask, setFilteredTask] = useState<any>([]);
     // TODO: the below code should removed when mutation for updating tasks is implemented
