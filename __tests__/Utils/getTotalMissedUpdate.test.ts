939d7c09be60246f2105ca34afd092b04ed4dd54
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-06-26 13:51:07 +0530

    seperated the file for pages and components folder

--- __tests__/Utils/getTotalMissedUpdate.test.ts
+++ __tests__/Utils/getTotalMissedUpdate.test.ts
@@ -13,13 +13,13 @@ describe('Get Total Missed Updates', () => {
     test('should return 1 if date array has last standup date', () => {
         const dateArrayValue = getMockedDates('25 june 2023');
         const result = getTotalMissedUpdates([dateArrayValue]);
-        expect(result).toBe(0);
+        expect(result).toBe(1);
     });
 
     test('should return number of days between last standup date and current standup date', () => {
-        const firstDate = getMockedDates('21 june 2023');
-        const SecondDate = getMockedDates('22 june 2023');
-        const ThirdDate = getMockedDates('23 june 2023');
+        const firstDate = getMockedDates('22 june 2023');
+        const SecondDate = getMockedDates('23 june 2023');
+        const ThirdDate = getMockedDates('24 june 2023');
         const result = getTotalMissedUpdates([
             firstDate,
             SecondDate,
