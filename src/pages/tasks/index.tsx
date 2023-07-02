7dbfccd75f934c94ec1922da527edc200d6aa618
Prakash <om.pcprakash@gmail.com>
2023-07-02 12:22:44 +0530

    update component name

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -4,7 +4,7 @@ import Layout from '@/components/Layout';
 import classNames from '@/styles/tasks.module.scss';
 import { TasksContent } from '@/components/tasks/TasksContent';
 
-const Task: FC = () => {
+const Tasks: FC = () => {
     return (
         <Layout>
             <Head title="Tasks" />
@@ -16,4 +16,4 @@ const Task: FC = () => {
     );
 };
 
-export default Task;
+export default Tasks;
