a7d13bd5a9636de45236ac93a3ef7c3eb7c2779c
WYGIN <wygininc@gmail.com>
2023-07-03 11:33:45 +0000

    added assignTask RTK

--- src/app/services/tasksApi.ts
+++ src/app/services/tasksApi.ts
@@ -1,10 +1,34 @@
 import task, { updateTaskDetails } from '@/interfaces/task.type';
 import { api } from './api';
 import { MINE_TASKS_URL, TASKS_URL } from '@/constants/url';
+import { ASSIGNED } from '@/constants/task-status';
+import {
+    THOUSAND_MILLI_SECONDS,
+    FOURTEEN_DAYS,
+    SECONDS_IN_A_DAY,
+} from '@/constants/date';
 
 type TasksQueryResponse = { message: string; tasks: task[] };
 type TasksCreateMutationResponse = { message: string; task: task };
 type TaskRequestPayload = { task: updateTaskDetails; id: string };
+type AssignTaskPayload = { taskId: string; assignee: string };
+
+export const assignTaskReducerStateBuilder = () => {
+    const dateObject: Date = new Date();
+    const startedOnEpoch: number =
+        dateObject.getTime() / THOUSAND_MILLI_SECONDS;
+    const endsOnEpoch: number =
+        startedOnEpoch + FOURTEEN_DAYS * SECONDS_IN_A_DAY;
+    const build = ({ assignee }: { assignee: string }) => {
+        return {
+            status: ASSIGNED,
+            startedOn: startedOnEpoch,
+            endsOn: endsOnEpoch,
+            assignee,
+        };
+    };
+    return build;
+};
 
 export const tasksApi = api.injectEndpoints({
     endpoints: (builder) => ({
@@ -51,6 +75,21 @@ export const tasksApi = api.injectEndpoints({
                 },
             ],
         }),
+        assignTask: builder.mutation<void, AssignTaskPayload>({
+            query: (payload: AssignTaskPayload) => ({
+                url: `tasks/${payload.taskId}`,
+                method: 'PATCH',
+                body: assignTaskReducerStateBuilder()({
+                    assignee: payload.assignee,
+                }),
+            }),
+            invalidatesTags: (_result, _err, { taskId }) => [
+                {
+                    type: 'Tasks',
+                    id: taskId,
+                },
+            ],
+        }),
     }),
     overrideExisting: true,
 });
@@ -60,4 +99,5 @@ export const {
     useGetMineTasksQuery,
     useAddTaskMutation,
     useUpdateTaskMutation,
+    useAssignTaskMutation,
 } = tasksApi;
