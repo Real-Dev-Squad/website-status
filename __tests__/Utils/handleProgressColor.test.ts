4dbe4d8ede470ea0e460a20984092a80da8da474
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-07-11 03:59:11 +0530

    added jest.fakeTimer to give fake system time to simulate and keep current date constant

--- __tests__/Utils/handleProgressColor.test.ts
+++ __tests__/Utils/handleProgressColor.test.ts
@@ -1,27 +1,40 @@
 import handleProgressColor from '@/utils/handleProgressColor';
 
 describe('handleProgressColor', () => {
+    beforeEach(() => {
+        jest.useFakeTimers().setSystemTime(new Date('10 july 2023'));
+    });
     test('should render green color', () => {
-        const colorName = handleProgressColor(100, '1688515200', '1689120000');
+        jest.useFakeTimers().setSystemTime(new Date('11 july 2023'));
+        //start date -july 7 2023 , end date - 17 July 2023
+        const colorName = handleProgressColor(100, '1688732501', '1689552000');
         expect(colorName).toBe('progressGreen');
     });
-    test('should render red color if 45 percent of task is complete', () => {
-        const colorName = handleProgressColor(45, '1688342400', '1688774400');
+    test('should render red color ', () => {
+        jest.useFakeTimers().setSystemTime(new Date('11 july 2023'));
+        //start date -july 8 2023 , end date - 11 July 2023
+        const colorName = handleProgressColor(45, '1688817600', '1689033600');
         expect(colorName).toBe('progressRed');
     });
 
     test('should render red color', () => {
-        const colorName = handleProgressColor(0, '1688515200', '1688601600');
+        jest.useFakeTimers().setSystemTime(new Date('11 july 2023'));
+        //start date -july 5 2023 , end date - 12 July 2023
+        const colorName = handleProgressColor(0, '1688515200', '1689163200');
         expect(colorName).toBe('progressRed');
     });
 
     test('should render Orange color', () => {
-        const colorName = handleProgressColor(0, '1688428800', '1688860800');
+        jest.useFakeTimers().setSystemTime(new Date('11 july 2023'));
+        //start date -july 9 2023 , end date - 12 July 2023
+        const colorName = handleProgressColor(10, '1688884200', '1689143400');
         expect(colorName).toBe('progressOrange');
     });
 
     test('should render Yellow color', () => {
-        const colorName = handleProgressColor(30, '1688428800', '1688860800');
+        jest.useFakeTimers().setSystemTime(new Date('11 july 2023'));
+        //start date -july 9 2023 , end date - 12 July 2023
+        const colorName = handleProgressColor(30, '1688884200', '1689143400');
         expect(colorName).toBe('progressYellow');
     });
 });
--- src/utils/getPercentageOfDaysLeft.ts
+++ src/utils/getPercentageOfDaysLeft.ts
@@ -4,12 +4,17 @@ const getPercentageOfDaysLeft = (startedOn: string, endsOn: string): number => {
     const startDate = moment(new Date(parseInt(startedOn, 10) * 1000));
     const endDate = moment(new Date(parseInt(endsOn, 10) * 1000));
 
-    // It provides us with total days that are there for the the project and
-    //number of days left
+    /**
+     * It provides us with total days that are there for the the project and
+     * number of days left
+     */
     const totalDays = endDate.diff(startDate, 'days');
     const daysLeft = endDate.diff(new Date(), 'days');
 
-    // It provides the percentage of days left
+    /**
+     * It provides the percentage of days left
+     */
+
     const percentageOfDaysLeft = (daysLeft / totalDays) * 100;
     return percentageOfDaysLeft;
 };
