32e3f86ee021439bd2f31d4fa6930811aaa39833
ritikjaiswal75 <ritik12103032000@gmail.com>
2023-07-20 10:16:50 +0530

    replace if else with switch case

--- src/utils/getActiveTab.ts
+++ src/utils/getActiveTab.ts
@@ -1,21 +1,22 @@
 import { Tab } from '@/interfaces/task.type';
 
 export const getActiveTab = (section?: string): Tab => {
-    if (section === 'assigned') {
-        return Tab.ASSIGNED;
-    } else if (section === 'available') {
-        return Tab.AVAILABLE;
-    } else if (section === 'needs_review') {
-        return Tab.NEEDS_REVIEW;
-    } else if (section === 'in_review') {
-        return Tab.IN_REVIEW;
-    } else if (section === 'verified') {
-        return Tab.VERIFIED;
-    } else if (section === 'merged') {
-        return Tab.MERGED;
-    } else if (section === 'completed') {
-        return Tab.COMPLETED;
-    } else {
-        return Tab.IN_PROGRESS;
+    switch (section) {
+        case 'assigned':
+            return Tab.ASSIGNED;
+        case 'available':
+            return Tab.AVAILABLE;
+        case 'needs_review':
+            return Tab.NEEDS_REVIEW;
+        case 'in_review':
+            return Tab.IN_REVIEW;
+        case 'verified':
+            return Tab.VERIFIED;
+        case 'merged':
+            return Tab.MERGED;
+        case 'completed':
+            return Tab.COMPLETED;
+        default:
+            return Tab.IN_PROGRESS;
     }
 };
