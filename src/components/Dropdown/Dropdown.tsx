b95bc4bd4ef12274e77b017b2ff86b9226a66e62
FMK2312 <fmkhandwala39@gmail.com>
2023-07-28 18:08:23 +0530

    changed designs and added new options

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -1,5 +1,10 @@
 import Link from 'next/link';
-import { LOGOUT_URL, USER_PROFILE_URL } from '../../constants/url';
+import {
+    LOGOUT_URL,
+    MAIN_SITE_URL,
+    MY_SITE_URL,
+    USER_PROFILE_URL,
+} from '../../constants/url';
 import styles from '@/components/Dropdown/dropdown.module.scss';
 
 const Dropdown = () => {
@@ -15,8 +20,29 @@ const Dropdown = () => {
     return (
         <div className={styles.dropdown}>
             <ul className={styles.dropdownList}>
+                <Link href={MAIN_SITE_URL} className={styles.dropdownLink}>
+                    <li className={styles.dropdownItem}>Home</li>
+                </Link>
                 <Link href={USER_PROFILE_URL} className={styles.dropdownLink}>
-                    <li className={styles.dropdownItem}>My Profile</li>
+                    <li className={styles.dropdownItem}>Status</li>
+                </Link>
+                <Link
+                    href={`${MY_SITE_URL}/profile`}
+                    className={styles.dropdownLink}
+                >
+                    <li className={styles.dropdownItem}>Profile</li>
+                </Link>
+                <Link
+                    href={`${MY_SITE_URL}/tasks`}
+                    className={styles.dropdownLink}
+                >
+                    <li className={styles.dropdownItem}>Tasks</li>
+                </Link>
+                <Link
+                    href={`${MY_SITE_URL}/identity`}
+                    className={styles.dropdownLink}
+                >
+                    <li className={styles.dropdownItem}>Identity</li>
                 </Link>
                 <hr className={styles.line} />
                 <li className={styles.dropdownItem} onClick={logout}>
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -26,3 +26,5 @@ export const ITEM_TYPES = { task: 'TASK' };
 export const TASKS_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/tasks`;
 export const ISSUES_URL = `${process.env.NEXT_PUBLIC_BASE_URL}/issues`;
 export const USERS_URL = `${BASE_URL}/users`;
+export const MAIN_SITE_URL = 'https://www.realdevsquad.com';
+export const MY_SITE_URL = 'https://my.realdevsquad.com';
