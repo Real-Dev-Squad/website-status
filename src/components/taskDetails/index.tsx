d2fd26e7d9d0a6c9440599bcf29f8f1e9f1c66ec
Tanishq Singla <tanishqsingla00@gmail.com>
2023-08-05 21:36:07 +0530

    feat: Add task request button to task details page (#730)
    * feat: Add button to request for task
    
    * fix: Review comments
    
    Add test for api
    
    * Rename taskRequestApi.tsx to taskRequestApi.test.tsx
    
    * fix: same test names
    
    ---------
    
    Co-authored-by: Sunny Sahsi <sahsisunny@gmail.com>

--- src/components/taskDetails/index.tsx
+++ src/components/taskDetails/index.tsx
@@ -27,6 +27,7 @@ import TaskDependency from '@/components/taskDetails/taskDependency';
 import { useGetProgressDetailsQuery } from '@/app/services/progressesApi';
 import { ProgressDetailsData } from '@/types/standup.type';
 import { getDateFromTimestamp } from '@/utils/getDateFromTimestamp';
+import { useAddOrUpdateMutation } from '@/app/services/taskRequestApi';
 
 export function Button(props: ButtonProps) {
     const { buttonName, clickHandler, value } = props;
@@ -63,7 +64,7 @@ const TaskDetails: FC<Props> = ({ taskID }) => {
     const { query } = router;
     const isDevModeEnabled = query.dev === 'true' ? true : false;
 
-    const { isUserAuthorized } = useUserData();
+    const { isUserAuthorized, data: userData } = useUserData();
 
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const { data, isError, isLoading, isFetching } =
@@ -79,6 +80,9 @@ const TaskDetails: FC<Props> = ({ taskID }) => {
     const [editedTaskDetails, setEditedTaskDetails] = useState<
         taskDetailsDataType['taskData'] | undefined
     >(data?.taskData);
+
+    const [addOrUpdateTaskRequest, taskRequestUpdateStatus] =
+        useAddOrUpdateMutation();
 
     useEffect(() => {
         if (data?.taskData) {
@@ -114,6 +118,16 @@ const TaskDetails: FC<Props> = ({ taskID }) => {
                 ? { [name]: name === 'dependsOn' ? [value] : value }
                 : {}),
         }));
+    }
+
+    function taskRequestHandle() {
+        if (!userData) {
+            return;
+        }
+        addOrUpdateTaskRequest({ taskId: taskID, userId: userData.id })
+            .unwrap()
+            .then(() => toast(SUCCESS, 'Successfully requested for task'))
+            .catch((error) => toast(ERROR, error.data.message));
     }
 
     function renderLoadingComponent() {
@@ -316,6 +330,14 @@ const TaskDetails: FC<Props> = ({ taskID }) => {
                                         taskDetailsData?.endsOn ?? 0
                                     )}
                                 />
+                            </TaskContainer>
+                            <TaskContainer
+                                hasImg={false}
+                                title="Request for task"
+                            >
+                                <button onClick={taskRequestHandle}>
+                                    Request for task
+                                </button>
                             </TaskContainer>
                         </section>
                     </section>
