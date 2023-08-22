c2e3157fea9ceb3fadb04a439b8f8efba73b7ad8
AnjaliDeshwani <deshwanianjali@gmail.com>
2023-08-23 03:07:52 +0530

    created separate component to show status and few other changes

--- src/components/tasks/card/ShowError.tsx
+++ src/components/tasks/card/ShowError.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image';
 import classNames from '@/components/tasks/card/card.module.scss';
+
 export const ShowError = () => (
-    <div className={classNames.selfAlignEnd} data-testid="checkmark">
-        <Image src="/error.png" alt="data saved" width={20} height={20} />
+    <div className={classNames.selfAlignEnd} data-testid="error">
+        <Image
+            src="/error.png"
+            alt="Failed to Save Data"
+            width={20}
+            height={20}
+        />
     </div>
 );
--- src/components/tasks/constants.ts
+++ src/components/tasks/constants.ts
@@ -1,6 +1,6 @@
 export const INITIAL_TASKS_LIMIT = 3;
 export const ADD_MORE_TASKS_LIMIT = 5;
 export const SEE_MORE = 'See More';
-export const PENDING = 'Pending';
-export const SAVED = 'Saved';
-export const ERROR_STATUS = 'Error';
+export const PENDING = 'PENDING';
+export const SAVED = 'SAVED';
+export const ERROR_STATUS = 'ERROR';
