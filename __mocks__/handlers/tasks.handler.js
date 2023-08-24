f3610bd2ff09d67074ea4a71e207da3589e5436a
Pratiyush Kumar <pratiyush.inv@gmail.com>
2023-08-24 16:50:53 +0530

    removed spaces from task handler file

--- __mocks__/handlers/tasks.handler.js
+++ __mocks__/handlers/tasks.handler.js
@@ -106,8 +106,8 @@ export const mineTasksHandler = [
 ];
 
 export const failedGetMineTask= rest.get(`${URL}/tasks/self`, (_, res, ctx) => {
-        return res(ctx.status(500), ctx.json(failedGetTasksResponse));
-    });
+    return res(ctx.status(500), ctx.json(failedGetTasksResponse));
+});
 
 export const mineTasksNoDataFoundHandler = rest.get(
     `${URL}/tasks/self`,
@@ -145,20 +145,4 @@ export const filterTaskHandler = rest.get(`${URL}/tasks`, (req, res, ctx) => {
         })
     );
 });
-
-export const failedfilterTaskHandler = rest.get(`${URL}/tasks`, (req, res, ctx) => {
-        const searchTerm = req.url.searchParams.get('q');
-        if (searchTerm === 'searchTerm:') {
-            return res(
-            ctx.status(404),
-            ctx.json(failedFilterTasksResponse)
-        );
-        }
-        return res();
-    });
-
-export const failedFilterTasksResponse = {
-    message: 'No task found.',
-    tasks: [],
-};
 export default taskHandlers;
