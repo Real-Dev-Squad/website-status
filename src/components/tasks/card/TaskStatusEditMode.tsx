7d6c519531deb9a12b38d4e0318a50ccfe6bcab5
AnjaliDeshwani <deshwanianjali@gmail.com>
2023-08-26 18:30:57 +0530

    fix: editmode of task card

--- src/components/tasks/card/TaskStatusEditMode.tsx
+++ src/components/tasks/card/TaskStatusEditMode.tsx
@@ -1,42 +1,79 @@
 import { BACKEND_TASK_STATUS } from '@/constants/task-status';
-import task from '@/interfaces/task.type';
+import task, { CardTaskDetails } from '@/interfaces/task.type';
+import { useState } from 'react';
+import classNames from '@/components/tasks/card/card.module.scss';
+import { PENDING, SAVED, ERROR_STATUS } from '../constants';
+import { useUpdateTaskMutation } from '@/app/services/tasksApi';
+import { useRouter } from 'next/router';
+import { StatusIndicator } from './StatusIndicator';
 
 type Props = {
     task: task;
-    updateTask: (changeId: string, changeObject: { status: string }) => void;
+    setEditedTaskDetails: React.Dispatch<React.SetStateAction<CardTaskDetails>>;
 };
 
 // TODO: remove this after fixing the card beautify status
 const beautifyStatus = (status: string) => status.split('_').join(' ');
 const taskStatus = Object.entries(BACKEND_TASK_STATUS);
 
-const TaskStatusEditMode = ({ task, updateTask }: Props) => {
+const TaskStatusEditMode = ({ task, setEditedTaskDetails }: Props) => {
+    const router = useRouter();
+    const { dev } = router.query;
+
+    const [saveStatus, setSaveStatus] = useState('');
+    const [updateTask] = useUpdateTaskMutation();
+
     const onChangeUpdateTaskStatus = ({
         target: { value },
     }: React.ChangeEvent<HTMLSelectElement>) => {
-        updateTask(task.id, {
+        setSaveStatus(PENDING);
+        setEditedTaskDetails((prev: CardTaskDetails) => ({
+            ...prev,
             status: value,
+        }));
+        const response = updateTask({
+            id: task.id,
+            task: {
+                status: value,
+            },
         });
+
+        response
+            .unwrap()
+            .then((result: any) => {
+                setSaveStatus(SAVED);
+            })
+            .catch((err: { data: { message: string } }) => {
+                setSaveStatus(ERROR_STATUS);
+            })
+            .finally(() => {
+                setTimeout(() => {
+                    setSaveStatus('');
+                }, 3000);
+            });
     };
 
     // TODO: remove this after fixing the card beautify status
     const defaultStatus = task.status.toUpperCase().split(' ').join('_');
 
     return (
-        <label>
-            Status:
-            <select
-                name="status"
-                onChange={onChangeUpdateTaskStatus}
-                defaultValue={defaultStatus}
-            >
-                {taskStatus.map(([name, status]) => (
-                    <option key={status} value={status}>
-                        {beautifyStatus(name)}
-                    </option>
-                ))}
-            </select>
-        </label>
+        <div className={classNames.taskSection}>
+            <label>
+                Status:
+                <select
+                    name="status"
+                    onChange={onChangeUpdateTaskStatus}
+                    defaultValue={defaultStatus}
+                >
+                    {taskStatus.map(([name, status]) => (
+                        <option key={status} value={status}>
+                            {beautifyStatus(name)}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            {dev === 'true' && <StatusIndicator status={saveStatus} />}
+        </div>
     );
 };
 
--- src/components/tasks/constants.ts
+++ src/components/tasks/constants.ts
@@ -1,3 +1,6 @@
 export const INITIAL_TASKS_LIMIT = 3;
 export const ADD_MORE_TASKS_LIMIT = 5;
 export const SEE_MORE = 'See More';
+export const PENDING = 'PENDING';
+export const SAVED = 'SAVED';
+export const ERROR_STATUS = 'ERROR';
