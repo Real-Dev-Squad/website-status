ecee5c4dec9b9ec50aa81f364eddd7d472b35c2f
Fakhruddin KW <85249044+fakhruddinkw@users.noreply.github.com>
2023-09-06 20:55:17 +0530

    Dev to main sync (#856)

--- __tests__/Unit/Components/ProgressForm/ProgressHeader.test.tsx
+++ __tests__/Unit/Components/ProgressForm/ProgressHeader.test.tsx
@@ -6,15 +6,8 @@ describe('ProgressHeader', () => {
     test('Should render Text in Progress Header component', () => {
         render(<ProgressHeader totalMissedUpdates={10} updateType="Standup" />);
         const missedUpdatesElement = screen.getByText(
-            'Let us try to avoid having zero days'
+            'Let us try not to miss giving updates'
         );
         expect(missedUpdatesElement).toBeInTheDocument();
-    });
-
-    test('Should render Standup Date and time', () => {
-        jest.useFakeTimers().setSystemTime(new Date('17 July 2023'));
-        render(<ProgressHeader totalMissedUpdates={10} updateType="Standup" />);
-        expect(screen.getByTestId('standupTime')).toBeInTheDocument();
-        expect(screen.getByTestId('currentStandupTime')).toBeInTheDocument();
     });
 });
