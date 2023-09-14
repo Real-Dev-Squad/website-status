456318052b3b72f230d1d44116a31d01f2e6f82a
Fakhruddin KW <85249044+fakhruddinkw@users.noreply.github.com>
2023-09-15 01:48:01 +0530

    Dev to main sync (#870)
    * created a separate component for showing updates
    
    * updated the type to include assigneeId
    
    * added condition, where task request will only be made if assignee is NA, also added condition so that only the user who is assigned can update the progress
    
    * Styles for progress-details
    
    * reverting the conditions for request task and update progress button
    
    * reverting taskDetail.types
    
    * added tests for ProgressDetails
    
    * made slight changes in styling
    
    * Fix: Display Link instead of button for git issue link in task details page. (#859)
    
    * Display link instead of button for git issue
    
    * used getByText screen method.
    
    * Followed url conventions, added regex in const
    
    ---------
    
    Co-authored-by: Vinit khandal <111434418+vinit717@users.noreply.github.com>
    
    * fixed appearence of list items to make it look clickable, also made the modal close button adhere to status site theme
    
    ---------
    
    Co-authored-by: Kapil Londhe <41846637+kapillondhe@users.noreply.github.com>
    Co-authored-by: Vinit khandal <111434418+vinit717@users.noreply.github.com>
    Co-authored-by: Amit Prakash <34869115+iamitprakash@users.noreply.github.com>

---
+++ src/components/taskDetails/ProgressDetails.tsx
@@ -0,0 +1,72 @@
+import { useState, FC } from 'react';
+import { ProgressDetailsData } from '@/types/standup.type';
+import { getDateFromTimestamp } from '@/utils/getDateFromTimestamp';
+import classNames from './progress-details.module.scss';
+
+type Props = {
+    data: ProgressDetailsData;
+};
+const ProgressDetails: FC<Props> = ({ data }) => {
+    const [showProgressDetails, setShowProgressDetails] =
+        useState<boolean>(false);
+
+    const openDetails = () => {
+        setShowProgressDetails(true);
+    };
+
+    const closeDetails = () => {
+        setShowProgressDetails(false);
+    };
+    return (
+        <>
+            <li
+                onClick={openDetails}
+                className={classNames['list-item']}
+                data-testid="progress-item"
+            >
+                {getDateFromTimestamp(data.date)}
+            </li>
+            {showProgressDetails && (
+                <div className={classNames['container-parent']}>
+                    <div
+                        role="button"
+                        className={classNames['container-back']}
+                        onClick={closeDetails}
+                        data-testid="modal-background"
+                    ></div>
+
+                    <div className={classNames['container-main']}>
+                        <h2>{getDateFromTimestamp(data.date)}</h2>
+                        <div
+                            className={classNames['content']}
+                            data-testid="content"
+                        >
+                            <div className={classNames['content-wrapper']}>
+                                <div className={classNames['content-item']}>
+                                    <span>Completed:</span>
+                                    <p>{data.completed}</p>
+                                </div>
+                                <div className={classNames['content-item']}>
+                                    <span>Planned:</span>
+                                    <p>{data.planned}</p>
+                                </div>
+                                <div className={classNames['content-item']}>
+                                    <span>Blockers:</span>
+                                    <p>{data.blockers}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <button
+                            className={classNames['container-close']}
+                            onClick={closeDetails}
+                        >
+                            Close
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
+
+export default ProgressDetails;
