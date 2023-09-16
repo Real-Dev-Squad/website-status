5d0f422a2c13ad28a57337e63e3731c248196b72
Amandixit10 <amandikshit10@gmail.com>
2023-09-17 00:46:07 +0530

    line break removed

--- __tests__/Utils/taskQueryParams.test.ts
+++ __tests__/Utils/taskQueryParams.test.ts
@@ -1,51 +1,42 @@
-import {
-    extractQueryParams,
-    getQueryParamTab,
-    getQueryParamAssignee,
-    getQueryParamTitle,
-} from '@/utils/taskQueryParams';
-
 import { Tab } from '@/interfaces/task.type';
+import { tabToUrlParams } from './getActiveTab';
 
-describe('extractQueryParams', () => {
-    it('should extract status, assignee, and title from query param', () => {
-        const queryParam =
-            'status:in-progress assignee:sunny-s Develop feature';
-        const result = extractQueryParams(queryParam);
-        expect(result.status).toBe('in-progress');
-        expect(result.assignee).toBe('sunny-s');
-        expect(result.title).toBe('Develop feature');
-    });
+export function extractQueryParams(queryParam: string) {
+    const queryParamsArray = queryParam?.split(' ');
 
-    it('should handle missing query param gracefully', () => {
-        const queryParam = '';
-        const result = extractQueryParams(queryParam);
-        expect(result.status).toBe('');
-        expect(result.assignee).toBe('');
-        expect(result.title).toBe('');
-    });
-});
+    let isValue = '';
+    let assigneeValue = '';
+    let titleValue = '';
 
-describe('getQueryParamTab', () => {
-    it('should generate a query param for a Tab', () => {
-        const tab = Tab.IN_PROGRESS;
-        const result = getQueryParamTab(tab);
-        expect(result).toBe('status:in-progress');
-    });
-});
+    if (!queryParamsArray) {
+        return {
+            status: isValue,
+            assignee: assigneeValue,
+            title: titleValue,
+        };
+    }
 
-describe('getQueryParamAssignee', () => {
-    it('should generate a query param for an assignee', () => {
-        const assignee = 'sunny-s';
-        const result = getQueryParamAssignee(assignee);
-        expect(result).toBe('assignee:sunny-s');
-    });
-});
+    for (let i = 0; i < queryParamsArray.length; i++) {
+        const param = queryParamsArray[i];
+        if (param.startsWith('status:')) {
+            isValue = param.replace('status:', '');
+        } else if (param.startsWith('assignee:')) {
+            assigneeValue = param.replace('assignee:', '');
+        } else {
+            titleValue += param + ' ';
+        }
+    }
 
-describe('getQueryParamTitle', () => {
-    it('should generate a query param for a title', () => {
-        const title = 'Develop feature';
-        const result = getQueryParamTitle(title);
-        expect(result).toBe('Develop feature');
-    });
-});
+    titleValue = titleValue.trim();
+
+    return {
+        status: isValue,
+        assignee: assigneeValue,
+        title: titleValue,
+    };
+}
+
+export const getQueryParamTab = (tab: Tab) => `status:${tabToUrlParams(tab)}`;
+export const getQueryParamAssignee = (assignee: string) =>
+    `assignee:${assignee}`;
+export const getQueryParamTitle = (title: string) => title;
