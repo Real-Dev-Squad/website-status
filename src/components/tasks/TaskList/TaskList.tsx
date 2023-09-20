64d0576b275c92531495fdc2229cf8771c983a0f
Sunny Sahsi <sahsisunny@gmail.com>
2023-09-20 19:15:08 +0530

    REFACTOR: removed old see more button from TaskList components (#772)
    * REFACTOR: removed unused code from tasklist components
    
    * TEST: remove unused test

--- src/components/tasks/TaskList/TaskList.tsx
+++ src/components/tasks/TaskList/TaskList.tsx
@@ -1,46 +1,14 @@
-import { useState } from 'react';
 import Card from '../card';
 import task from '@/interfaces/task.type';
-import beautifyTaskStatus from '@/helperFunctions/beautifyTaskStatus';
-import {
-    SEE_MORE,
-    INITIAL_TASKS_LIMIT,
-    ADD_MORE_TASKS_LIMIT,
-} from '../constants';
 import styles from '../card/card.module.scss';
 import { useUpdateTaskMutation } from '@/app/services/tasksApi';
 
 type TaskListProps = {
     tasks: task[];
-    hasLimit?: boolean;
 };
 
-type FilterTasksProps = {
-    hasLimit?: boolean;
-    tasksLimit: number;
-    tasks: task[];
-};
-
-function getFilteredTasks({ hasLimit, tasksLimit, tasks }: FilterTasksProps) {
-    if (!hasLimit) return tasks;
-    return tasks.slice(0, tasksLimit);
-}
-
-export default function TaskList({ tasks, hasLimit = false }: TaskListProps) {
-    const initialTasksLimit = hasLimit ? INITIAL_TASKS_LIMIT : tasks.length;
-    const beautifiedTasks = beautifyTaskStatus(tasks);
-    const [tasksLimit, setTasksLimit] = useState<number>(initialTasksLimit);
-    const filteredTasks = getFilteredTasks({
-        tasks: beautifiedTasks,
-        hasLimit,
-        tasksLimit,
-    });
-
+export default function TaskList({ tasks }: TaskListProps) {
     const [updateCardContent] = useUpdateTaskMutation();
-
-    function onSeeMoreTasksHandler() {
-        setTasksLimit((prevLimit) => prevLimit + ADD_MORE_TASKS_LIMIT);
-    }
     async function onContentChangeHandler(id: string, cardDetails: any) {
         if (!updateCardContent) return;
         updateCardContent({ id, task: cardDetails });
@@ -48,7 +16,7 @@ export default function TaskList({ tasks, hasLimit = false }: TaskListProps) {
 
     return (
         <div className={styles.taskCardsContainer}>
-            {filteredTasks.map((item: task) => (
+            {tasks.map((item: task) => (
                 <Card
                     content={item}
                     key={item.id}
@@ -56,15 +24,6 @@ export default function TaskList({ tasks, hasLimit = false }: TaskListProps) {
                     onContentChange={onContentChangeHandler}
                 />
             ))}
-            {hasLimit && filteredTasks.length != beautifiedTasks.length && (
-                <button
-                    type="button"
-                    onClick={onSeeMoreTasksHandler}
-                    className={styles.seeMoreTasks}
-                >
-                    {SEE_MORE}
-                </button>
-            )}
         </div>
     );
 }
