63a3893576ecdd90641bdd91386e1734f44e3a86
Aneena John Xavier <aneena_john_xavier@epam.com>
2023-10-19 19:26:57 +0530

    Removed commented tests
    Signed-off-by: Aneena John Xavier <aneena_john_xavier@epam.com>

--- __tests__/Unit/Components/Tasks/TasksContent.test.tsx
+++ __tests__/Unit/Components/Tasks/TasksContent.test.tsx
@@ -47,28 +47,6 @@ describe('tasks content', () => {
         await screen.findByTestId('tabs');
     });
 
-    // test('select tab and set active', async () => {
-    //     setWindowInnerWidth(breakpointToShowTabs);
-    //     renderWithRouter(
-    //         <Provider store={store()}>
-    //             <TasksContent dev={true} />
-    //         </Provider>,
-    //         {
-    //             query: { section: 'assigned' },
-    //         }
-    //     );
-    //     await screen.findByTestId('tabs');
-    //     const tabsContainer = within(
-    //         screen.getByTestId('status-tabs-container')
-    //     );
-    //     const assignedButton = tabsContainer.getByRole('button', {
-    //         name: 'ASSIGNED',
-    //     });
-    //     expect(assignedButton).toHaveTextContent('ASSIGNED');
-    //     await screen.findByText(NO_TASKS_FOUND_MESSAGE);
-    //     expect(screen.getByText(NO_TASKS_FOUND_MESSAGE)).toBeInTheDocument();
-    // });
-
     test('select tab and set active when dev is true', async () => {
         setWindowInnerWidth(breakpointToShowTabs);
         renderWithRouter(
@@ -129,36 +107,6 @@ describe('tasks content', () => {
         );
         expect(task).toBeInTheDocument();
     });
-
-    // test('Selecting a tab pushes into query params', async () => {
-    //     setWindowInnerWidth(breakpointToShowTabs);
-    //     const mockPushFunction = jest.fn();
-    //     renderWithRouter(
-    //         <Provider store={store()}>
-    //             <TasksContent />
-    //         </Provider>,
-    //         { push: mockPushFunction }
-    //     );
-
-    //     await screen.findByTestId('tabs');
-    //     const tabsContainer = within(
-    //         screen.getByTestId('status-tabs-container')
-    //     );
-    //     const inProgressBtn = tabsContainer.getByRole('button', {
-    //         name: /IN PROGRESS/i,
-    //     });
-    //     expect(inProgressBtn).toHaveClass('tabButton');
-    //     const unassignedButton = tabsContainer.getByRole('button', {
-    //         name: /UNASSIGNED/i,
-    //     });
-    //     fireEvent.click(unassignedButton);
-    //     expect(mockPushFunction).toBeCalledTimes(1);
-    //     expect(mockPushFunction).toBeCalledWith({
-    //         query: {
-    //             q: 'status:available',
-    //         },
-    //     });
-    // });
 
     test('Selecting a tab pushes into query params when dev is true', async () => {
         setWindowInnerWidth(breakpointToShowTabs);
@@ -221,40 +169,6 @@ describe('tasks content', () => {
 
         expect(selectStyles.display).toBe('block');
     });
-
-    // test('Selecting a value from dropdown pushes into query params', async () => {
-    //     setWindowInnerWidth(breakpointToShowSelect);
-    //     const mockPushFunction = jest.fn();
-    //     renderWithRouter(
-    //         <Provider store={store()}>
-    //             <TasksContent />
-    //         </Provider>,
-    //         { push: mockPushFunction }
-    //     );
-
-    //     await screen.findByTestId('status-select-container');
-    //     const selectContainer = screen?.getByTestId(
-    //         'selected-option-container'
-    //     );
-
-    //     fireEvent.click(selectContainer);
-    //     fireEvent.keyDown(selectContainer, {
-    //         key: 'ArrowDown',
-    //         code: 'ArrowDown',
-    //     });
-
-    //     fireEvent.keyDown(selectContainer, {
-    //         key: 'Enter',
-    //         code: 'Enter',
-    //     });
-
-    //     expect(mockPushFunction).toBeCalledTimes(1);
-    //     expect(mockPushFunction).toBeCalledWith({
-    //         query: {
-    //             q: 'status:in-progress',
-    //         },
-    //     });
-    // });
 
     test('Selecting a value from dropdown pushes into query params when dev is true', async () => {
         setWindowInnerWidth(breakpointToShowSelect);
