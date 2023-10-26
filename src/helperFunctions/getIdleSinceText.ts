738a73092dc9b73af682fb15937d441654a338cc
Anish Pawaskar <anishpawaskar@gmail.com>
2023-10-27 03:21:03 +0530

    fix getIdleSinceText function

--- src/helperFunctions/getIdleSinceText.ts
+++ src/helperFunctions/getIdleSinceText.ts
@@ -1,28 +1,26 @@
-import {
-    TOTAL_MILLISECONDS_IN_A_DAY,
-    TOTAL_MILLISECONDS_IN_A_HOUR,
-} from '@/constants/date';
+import { TOTAL_MILLISECONDS_IN_A_HOUR } from '@/constants/date';
 
 const getIdleSinceText = (idleSince: string) => {
     const presentDate = new Date();
     const idleSinceDate = new Date(idleSince);
-    const differenceInDay = Math.round(
-        (presentDate.setUTCHours(0, 0, 0, 0) -
-            idleSinceDate.setUTCHours(0, 0, 0, 0)) /
-            TOTAL_MILLISECONDS_IN_A_DAY
-    );
-    const differenceInHours = Math.abs(
-        Math.round(
-            (presentDate.getTime() - parseInt(idleSince)) /
-                TOTAL_MILLISECONDS_IN_A_HOUR
-        )
-    );
 
-    if (differenceInDay > 1) {
-        return `${differenceInDay} days ago`;
-    } else {
+    const presentDateInUtc = presentDate.toUTCString();
+    const idleSinceDateInUtc = idleSinceDate.toUTCString();
+
+    const differenceInHours = Math.round(
+        (new Date(presentDateInUtc).valueOf() -
+            new Date(idleSinceDateInUtc).valueOf()) /
+            TOTAL_MILLISECONDS_IN_A_HOUR
+    );
+    const idealDays = Math.round(differenceInHours / 24);
+
+    if (idealDays <= 0) {
         return `${differenceInHours} hours ago`;
+    } else if (idealDays === 1) {
+        return `${idealDays} day ago`;
     }
+
+    return `${idealDays} days ago`;
 };
 
 export default getIdleSinceText;
