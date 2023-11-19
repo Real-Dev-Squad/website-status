dcd2a9a489f65d725a956759b5f456dc7acdc3bd
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-11-20 04:16:43 +0530

    revert changes

--- __tests__/Unit/Components/Tasks/FilterModal.test.tsx
+++ __tests__/Unit/Components/Tasks/FilterModal.test.tsx
@@ -129,9 +129,10 @@ describe('FilterModal', () => {
         expect(inProgressButton).not.toHaveClass('status-button-active');
     });
 
-    test('renders the modal with correct active tab', () => {
+    test('renders the modal with correct active tab when dev is true', () => {
         render(
             <FilterModal
+                dev={true}
                 tabs={[Tab.UNASSIGNED, Tab.DONE]}
                 onSelect={mockOnSelect}
                 activeTab={Tab.DONE}
