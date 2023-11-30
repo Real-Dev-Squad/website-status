00e037f02459ec002ee77829ed2e0daef40f1780
Joy <joygupta501@gmail.com>
2023-11-30 21:45:51 +0530

    Feat : Added new helper for task search
    Desc : Helper to convert query string to pill objects

---
+++ src/utils/convertStringToOptions.ts
@@ -0,0 +1,28 @@
+import { IOption } from '@/interfaces/searchOptions.type';
+import { extractQueryParams } from './taskQueryParams';
+export default function convertStringToOptions(
+    userString: string
+): Array<IOption> {
+    const convertedObject = extractQueryParams(userString);
+    let convertedArray = [];
+    if (convertedObject?.title) {
+        convertedArray.push({
+            title: convertedObject.title,
+        });
+    }
+    if (
+        convertedObject?.status &&
+        convertedObject.status.toLocaleLowerCase() !== 'all'
+    ) {
+        convertedArray.push({
+            status: convertedObject.status,
+        });
+    }
+    if (convertedObject.assignees.length > 0) {
+        convertedArray = [
+            ...convertedArray,
+            ...convertedObject.assignees.map((name) => ({ assignee: name })),
+        ];
+    }
+    return convertedArray;
+}
