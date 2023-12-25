19e7789d36c93c062274f86abfe2f6bb5c9bbfe0
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-25 19:36:45 +0530

    fix: 404 page like added images and footer from js to tsx

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import classNames from '@/components/footer/footer.module.scss';
+import Link from 'next/link';
 
 const Footer = () => (
     <footer className={classNames.infoRepo}>
         <p>
             The contents of this website are deployed from this{' '}
-            <a
+            <Link
                 href="https://github.com/Real-Dev-Squad/website-status"
                 target="_blank"
                 rel="noopener noreferrer"
             >
                 open sourced repo
-            </a>
+            </Link>
         </p>
     </footer>
 );
--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,13 +1,23 @@
 import Link from 'next/link';
 import Layout from '@/components/Layout';
-import styles from '@/components/Layout/Layout.module.scss';
+import styles from '@/pages/404/404.module.scss';
+import Image from 'next/image';
 
 export default function PageNotFound() {
     return (
         <Layout>
             <Link href="/" passHref>
-                <div className={styles.header}>
-                    <h2 className={styles.link}>404 - Page Not Found</h2>
+                <div className={styles.notFound}>
+                    <Image
+                        src="/not-found.jpg"
+                        alt="404 - Page Not Found"
+                        width={500}
+                        height={500}
+                        className={styles.notFoundImage}
+                    />
+                    <h2 className={styles.notFoundText}>
+                        404 - Page Not Found
+                    </h2>
                 </div>
             </Link>
         </Layout>
