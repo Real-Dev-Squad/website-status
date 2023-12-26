051a304959e661b6e395b6129fa343b6d61fe4b8
Sunny Sahsi <sahsisunny@gmail.com>
2023-12-26 18:12:14 +0530

    fix: change image and removed Link

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Layout from '@/components/Layout';
 import styles from '@/pages/404/404.module.scss';
 import Image from 'next/image';
@@ -6,20 +5,16 @@ import Image from 'next/image';
 export default function PageNotFound() {
     return (
         <Layout>
-            <Link href="/" passHref>
-                <div className={styles.notFound}>
-                    <Image
-                        src="/not-found.jpg"
-                        alt="404 - Page Not Found"
-                        width={500}
-                        height={500}
-                        className={styles.notFoundImage}
-                    />
-                    <h2 className={styles.notFoundText}>
-                        404 - Page Not Found
-                    </h2>
-                </div>
-            </Link>
+            <div className={styles.notFound}>
+                <Image
+                    src="/not-found.png"
+                    alt="404 - Page Not Found"
+                    width={500}
+                    height={500}
+                    className={styles.notFoundImage}
+                />
+                <h2 className={styles.notFoundText}>404 - Page Not Found</h2>
+            </div>
         </Layout>
     );
 }
