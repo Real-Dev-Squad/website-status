d126de71f04181b61e80971732b56c6ea9d31e7c
Satyam <bajpai1973satyam@gmail.com>
2023-12-28 14:34:11 +0530

    tests: add test case for tooltip show and hide

--- __tests__/Unit/Components/Tasks/ProgressUpdateCard/LatestProgressUpdateCard.test.tsx
+++ __tests__/Unit/Components/Tasks/ProgressUpdateCard/LatestProgressUpdateCard.test.tsx
@@ -1,12 +1,12 @@
-import { screen } from '@testing-library/react';
+import moment from 'moment';
+import { fireEvent, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '@/app/store';
 import { renderWithRouter } from '@/test_utils/createMockRouter';
 import { mockGetTaskProgress } from '../../../../../__mocks__/db/progresses';
 import LatestProgressUpdateCard from '@/components/taskDetails/ProgressUpdateCard/LatestProgressUpdateCard';
-import moment from 'moment';
 
-describe.only('LatestProgressUpdateCard Component', () => {
+describe('LatestProgressUpdateCard Component', () => {
     it('should render the component with the passed data', () => {
         renderWithRouter(
             <Provider store={store()}>
@@ -23,6 +23,7 @@ describe.only('LatestProgressUpdateCard Component', () => {
 
         const date = screen.getByTestId('latest-progress-update-card-date');
 
+        //TODO : uncomment when remove dummyLongString
         // expect(completedString).toBe(mockGetTaskProgress.data[2].completed);
         // expect(plannedString).toBe(mockGetTaskProgress.data[2].planned);
         // expect(blockersString).toBe(mockGetTaskProgress.data[2].blockers);
@@ -41,5 +42,33 @@ describe.only('LatestProgressUpdateCard Component', () => {
         const date = screen.getByTestId('latest-progress-update-card-date');
 
         expect(date.textContent).toBe(dateInAgoFormat);
+    });
+
+    it('should render the tooltip on hover on the date and should not render on mouse out off date', () => {
+        renderWithRouter(
+            <Provider store={store()}>
+                <LatestProgressUpdateCard data={mockGetTaskProgress.data[2]} />
+            </Provider>
+        );
+
+        const momentDate = moment(mockGetTaskProgress.data[2].date);
+        const fullDate = momentDate.format('DD-MM-YY');
+        const time = momentDate.format('hh:mmA');
+
+        const tooltipString = `Updated at ${fullDate}, ${time}`;
+        const dateElement = screen.getByTestId(
+            'latest-progress-update-card-date'
+        );
+
+        fireEvent.mouseOver(dateElement);
+
+        const tooltip = screen.getByTestId('tooltip');
+
+        expect(tooltip).toBeInTheDocument();
+        expect(tooltip.textContent).toBe(tooltipString);
+
+        fireEvent.mouseOut(dateElement);
+
+        expect(tooltip).not.toBeInTheDocument();
     });
 });
