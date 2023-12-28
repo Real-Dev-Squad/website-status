fa8c4b9e6502319e09e449bc4618cac64844ef18
Satyam <bajpai1973satyam@gmail.com>
2023-12-28 13:59:15 +0530

    feat: add more or less button

--- src/components/taskDetails/ProgressUpdateCard/LatestProgressUpdateCard.tsx
+++ src/components/taskDetails/ProgressUpdateCard/LatestProgressUpdateCard.tsx
@@ -22,11 +22,12 @@ export default function LatestProgressUpdateCard({
     const fullDate = momentDate.format('DD-MM-YY');
     const time = momentDate.format('hh:mmA');
     const tooltipString = `Updated at ${fullDate}, ${time}`;
+    const charactersToShow = 50;
     const [readMoreState, setReadMoreState] = useState<ReadMoreState>({
         isReadMoreEnabled: false,
         enabledFor: '',
     });
-
+    const moreOrLessText = 'More';
     function onHoverOnDate(e: MouseEvent<HTMLElement>) {
         setIsTooltipVisible(true);
     }
@@ -34,63 +35,61 @@ export default function LatestProgressUpdateCard({
     function onMouseOutOnDate(e: MouseEvent<HTMLElement>) {
         setIsTooltipVisible(false);
     }
+    const dummyLongString =
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. ';
+
+    const dataToShow = [
+        {
+            id: `completed-${data.id}`,
+            label: 'Completed:',
+            // body: data.completed,
+            // shouldReadMoreButtonShow: data.completed.length > charactersToShow,
+            body: dummyLongString,
+            shouldReadMoreButtonShow: dummyLongString.length > charactersToShow,
+        },
+        {
+            id: `planned-${data.id}`,
+            label: 'Planned:',
+            body: data.planned,
+            shouldReadMoreButtonShow: data.planned.length > charactersToShow,
+        },
+        {
+            id: `blockers-${data.id}`,
+            label: 'Blockers:',
+            body: data.blockers,
+            shouldReadMoreButtonShow: data.blockers.length > charactersToShow,
+        },
+    ];
+
+    const progressInfoMapping = dataToShow.map((datum) => (
+        <div
+            key={datum.id}
+            className={styles['latest-progress-update-card__info-container']}
+        >
+            <span className={styles['latest-progress-update-card__info-title']}>
+                {datum.label}
+            </span>
+            <span className={styles['latest-progress-update-card__info-body']}>
+                {datum.body}
+
+                {datum.shouldReadMoreButtonShow && (
+                    <button
+                        className={
+                            styles[
+                                'latest-progress-update-card__more-less-button'
+                            ]
+                        }
+                    >
+                        {moreOrLessText}
+                    </button>
+                )}
+            </span>
+        </div>
+    ));
 
     return (
         <div className={styles['latest-progress-update-card']}>
-            <div
-                className={
-                    styles['latest-progress-update-card__info-container']
-                }
-            >
-                <span
-                    className={
-                        styles['latest-progress-update-card__info-title']
-                    }
-                >
-                    Completed:
-                </span>
-                <span
-                    className={styles['latest-progress-update-card__info-body']}
-                >
-                    {data.completed}
-                </span>
-            </div>
-            <div
-                className={
-                    styles['latest-progress-update-card__info-container']
-                }
-            >
-                <span
-                    className={
-                        styles['latest-progress-update-card__info-title']
-                    }
-                >
-                    Planned:
-                </span>
-                <span
-                    className={styles['latest-progress-update-card__info-body']}
-                >
-                    {data.planned}
-                </span>
-            </div>
-            <div
-                className={
-                    styles['latest-progress-update-card__info-container']
-                }
-            >
-                <span
-                    className={
-                        styles['latest-progress-update-card__info-title']
-                    }
-                >
-                    Blockers:
-                </span>
-                <span
-                    className={styles['latest-progress-update-card__info-body']}
-                >
-                    {data.blockers}
-                </span>
-            </div>
+            {progressInfoMapping}
             <div
                 className={
                     styles['latest-progress-update-card__info-container']
