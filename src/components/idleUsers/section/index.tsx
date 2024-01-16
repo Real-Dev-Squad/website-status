f31f98562b5ee55a1abb9afb000da66633fe7cd2
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2024-01-16 16:27:05 +0530

    use global SCSS variables in /idle-users

--- src/components/idleUsers/section/index.tsx
+++ src/components/idleUsers/section/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Card from '@/components/idleUsers/card';
-import classNames from '@/components/idleUsers/section/section.module.scss';
+import styles from '@/components/idleUsers/section/section.module.scss';
 import { UserStatusArray, UserStatus } from '@/interfaces/userStatus.type';
 
 function renderCards(content: UserStatus[]) {
@@ -15,19 +15,17 @@ const Section: FC<UserStatusArray> = ({
     error,
     isLoading,
 }) => (
-    <div className={classNames.section}>
-        <div className={classNames.heading}>{heading}</div>
+    <div className={styles.section}>
+        <div className={styles.heading}>{heading}</div>
         {!!error && (
-            <span className={classNames.statusMessage}>
+            <span className={styles.statusMessage}>
                 Something went wrong, please contact admin!
             </span>
         )}
         {isLoading ? (
-            <span className={classNames.statusMessage}>Loading...</span>
+            <span className={styles.statusMessage}>Loading...</span>
         ) : (
-            <div className={classNames.cardContainer}>
-                {renderCards(content)}
-            </div>
+            <div className={styles.cardContainer}>{renderCards(content)}</div>
         )}
     </div>
 );
