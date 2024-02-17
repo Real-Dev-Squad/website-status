0d577e347b05fe4fe41928b483a4426e9ba33a2a
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-02-17 22:15:35 +0530

    feat: Implement human-readable date and time format in TaskDetails component

--- src/helperFunctions/convertTimeStamp.ts
+++ src/helperFunctions/convertTimeStamp.ts
@@ -1,6 +1,22 @@
-const convertTimeStamp = (timeStamp: number) => {
+/*const convertTimeStamp = (timeStamp: number) => {
     const dateTime = new Date(timeStamp * 1000);
     return dateTime.toLocaleString();
+};
+
+export default convertTimeStamp;
+*/
+
+const convertTimeStamp = (timeStamp: number): string => {
+    const dateTime = new Date(timeStamp * 1000);
+    const options: Intl.DateTimeFormatOptions = {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+    };
+    return dateTime.toLocaleDateString('en-US', options);
 };
 
 export default convertTimeStamp;
