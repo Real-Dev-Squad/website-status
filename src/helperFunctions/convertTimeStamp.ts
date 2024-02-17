5f6eb9407aeea977802b91207e6a9c3e6c32ae4d
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-02-17 22:20:14 +0530

    chore:remove commented code

--- src/helperFunctions/convertTimeStamp.ts
+++ src/helperFunctions/convertTimeStamp.ts
@@ -1,11 +1,3 @@
-/*const convertTimeStamp = (timeStamp: number) => {
-    const dateTime = new Date(timeStamp * 1000);
-    return dateTime.toLocaleString();
-};
-
-export default convertTimeStamp;
-*/
-
 const convertTimeStamp = (timeStamp: number): string => {
     const dateTime = new Date(timeStamp * 1000);
     const options: Intl.DateTimeFormatOptions = {
