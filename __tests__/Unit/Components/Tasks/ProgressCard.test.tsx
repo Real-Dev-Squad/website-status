e1bf2069d71868254f1cafeb2ad0a38d5420935d
Vinayak <myselfvinayakhegde@gmail.com>
2024-02-22 14:01:02 +0530

    fix ascending and descending order tests

--- __tests__/Unit/Components/Tasks/ProgressCard.test.tsx
+++ __tests__/Unit/Components/Tasks/ProgressCard.test.tsx
@@ -2,12 +2,22 @@ import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '@/app/store';
 import { renderWithRouter } from '@/test_utils/createMockRouter';
+import moment from 'moment';
 import { setupServer } from 'msw/node';
 import handlers from '../../../../__mocks__/handlers';
 import { mockGetTaskProgress } from '../../../../__mocks__/db/progresses';
 import ProgressCard from '@/components/ProgressCard';
 
 const server = setupServer(...handlers);
+
+const dateMay29 = moment(mockGetTaskProgress.data[0].createdAt).fromNow();
+const dateMay27 = moment(mockGetTaskProgress.data[1].createdAt).fromNow();
+const dateMay31_122002 = moment(
+    mockGetTaskProgress.data[2].createdAt
+).fromNow();
+const dateMay31_122006 = moment(
+    mockGetTaskProgress.data[3].createdAt
+).fromNow();
 
 beforeAll(() => {
     server.listen({
@@ -54,13 +64,17 @@ describe('ProgressCard Component', () => {
                 <ProgressCard taskProgress={mockGetTaskProgress.data} />
             </Provider>
         );
+        const latestUpdateDate = container.querySelector(
+            '[data-testid="latest-progress-update-card-date"]'
+        );
         const progressArr = container.querySelectorAll(
-            '[data-testid="tooltip"]'
+            '[data-testid="progress-update-card-date"]'
         );
 
-        expect(progressArr[0].textContent).toMatch(/^Updated at 31-05-23/);
-        expect(progressArr[1].textContent).toMatch(/^Updated at 31-05-23/);
-        expect(progressArr[2].textContent).toMatch(/^Updated at 29-05-23/);
+        expect(latestUpdateDate).toHaveTextContent(dateMay31_122006);
+        expect(progressArr[0]).toHaveTextContent(dateMay31_122002);
+        expect(progressArr[1]).toHaveTextContent(dateMay29);
+        expect(progressArr[2]).toHaveTextContent(dateMay27);
     });
     it('should render the progress enteries in dscending order after Dsc btn click  ', async () => {
         const { container, getByRole } = renderWithRouter(
@@ -71,11 +85,16 @@ describe('ProgressCard Component', () => {
 
         const btn = getByRole('button', { name: 'Dsc' });
         fireEvent.click(btn);
-        const progressArr = container.querySelectorAll(
-            '[data-testid="tooltip"]'
+        const latestUpdateDate = container.querySelector(
+            '[data-testid="latest-progress-update-card-date"]'
         );
-        expect(progressArr[0].textContent).toMatch(/^Updated at 27-05-23/);
-        expect(progressArr[1].textContent).toMatch(/^Updated at 29-05-23/);
-        expect(progressArr[2].textContent).toMatch(/^Updated at 31-05-23/);
+        const progressArr = container.querySelectorAll(
+            '[data-testid="progress-update-card-date"]'
+        );
+
+        expect(progressArr[0]).toHaveTextContent(dateMay27);
+        expect(progressArr[1]).toHaveTextContent(dateMay29);
+        expect(progressArr[2]).toHaveTextContent(dateMay31_122002);
+        expect(latestUpdateDate).toHaveTextContent(dateMay31_122006);
     });
 });
