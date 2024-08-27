dd2e0ea9fd6096befc46abcca7be9eab76eecaac
Shubham Sharma <shbh541@gmail.com>
2024-08-27 23:17:35 +0530

    Added catch try catch block.

--- __mocks__/handlers/task-details.handler.ts
+++ __mocks__/handlers/task-details.handler.ts
@@ -3,42 +3,46 @@ const URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 const taskDetailsHandler = [
     rest.get(`${URL}/tasks/12345/details`, (_, res, ctx) => {
-        return res(
-            ctx.status(200),
-            ctx.json({
-                message: 'task returned successfully',
-                taskData: {
-                    id: '12345',
-                    isNoteworthy: true,
-                    lossRate: {
-                        dinero: 0,
-                        neelam: 0,
-                    },
-                    purpose: 'This is a sample description',
-                    endsOn: 1618790410,
-                    title: 'test 1 for drag and drop',
-                    status: 'COMPLETED',
-                    assignee: 'ankur',
-                    links: [],
-                    dependsOn: [],
-                    percentCompleted: 0,
-                    type: 'feature',
-                    priority: 'high',
-                    featureUrl: 'https://www.sampleUrl.com',
-                    startedOn: 1617062400,
-                    completionAward: {
-                        neelam: 0,
-                        dinero: 110,
-                    },
-                    github: {
-                        issue: {
-                            html_url:
-                                'https://github.com/sample-org/sample-repo/issues/000',
+        try {
+            return res(
+                ctx.status(200),
+                ctx.json({
+                    message: 'task returned successfully',
+                    taskData: {
+                        id: '12345',
+                        isNoteworthy: true,
+                        lossRate: {
+                            dinero: 0,
+                            neelam: 0,
+                        },
+                        purpose: 'This is a sample description',
+                        endsOn: 1618790410,
+                        title: 'test 1 for drag and drop',
+                        status: 'COMPLETED',
+                        assignee: 'ankur',
+                        links: [],
+                        dependsOn: [],
+                        percentCompleted: 0,
+                        type: 'feature',
+                        priority: 'high',
+                        featureUrl: 'https://www.sampleUrl.com',
+                        startedOn: 1617062400,
+                        completionAward: {
+                            neelam: 0,
+                            dinero: 110,
+                        },
+                        github: {
+                            issue: {
+                                html_url:
+                                    'https://github.com/sample-org/sample-repo/issues/000',
+                            },
                         },
                     },
-                },
-            })
-        );
+                })
+            );
+        } catch (error) {
+            return res(ctx.status(500), ctx.json({ error }));
+        }
     }),
     rest.get(`${URL}/tasks/6KhcLU3yr45dzjQIVm0J/details`, (_, res, ctx) => {
         return res(
