609cd841533dca8d9b67a660730b7c63c1a2f626
Pankaj Sha <pankajshadev@gmail.com>
2024-11-25 00:18:50 +0530

    fix: change shimmer card style and made it similar to task card

--- src/components/Loaders/taskCardShimmer/index.tsx
+++ src/components/Loaders/taskCardShimmer/index.tsx
@@ -2,10 +2,13 @@ import styles from './taskCardShimmer.module.scss';
 
 const TaskCardShimmer = () => (
     <div className={`${styles.taskCard}`} data-testid="task-shimmer-card">
-        <div className={`${styles.title} ${styles.animate}`} />
-        <div className={`${styles.title} ${styles.animate}`} />
-        <div className={`${styles.title} ${styles.animate}`} />
-        <div className={`${styles.title} ${styles.animate}`} />
+        <div className={`${styles.title} ${styles.animate}`}></div>
+        <div className={`${styles.flexContainer}`}>
+            <div className={`${styles.estimated} ${styles.animate}`}></div>
+            <div className={`${styles.status} ${styles.animate}`}></div>
+        </div>
+        <div className={`${styles.started} ${styles.animate}`} />
+        <div className={`${styles.assignee} ${styles.animate}`} />
     </div>
 );
 
