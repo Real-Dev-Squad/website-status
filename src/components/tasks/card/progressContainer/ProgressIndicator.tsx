e0287a0c457eeee906aaa6a92563e5bfef364495
Achintya-Chatterjee <achintyachatterjee.jara@gmail.com>
2024-12-19 17:32:16 +0530

    chore: remove feature flag from the new progress update flow modal

--- src/components/tasks/card/progressContainer/ProgressIndicator.tsx
+++ src/components/tasks/card/progressContainer/ProgressIndicator.tsx
@@ -3,22 +3,19 @@ import { FC } from 'react';
 import styles from '@/components/tasks/card/card.module.scss';
 import { ProgressIndicatorProps } from '@/interfaces/task.type';
 import handleProgressColor from '@/utils/handleProgressColor';
-import { useRouter } from 'next/router';
 
 const ProgressIndicator: FC<ProgressIndicatorProps> = ({
     percentCompleted,
     startedOn,
     endsOn,
 }) => {
-    const { query } = useRouter();
-    const isDev = query.dev === 'true';
     const progressColor = handleProgressColor(
         percentCompleted,
         startedOn,
         endsOn
     );
     return (
-        <div className={isDev ? styles.slider : styles.progressIndicator}>
+        <div className={styles.slider}>
             <div
                 className={`
                 ${progressColor}
