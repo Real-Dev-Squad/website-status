c3a1a0b6da73ca325b4c72b0cab73eff5e96ddeb
Achintya Chatterjee <55826451+Achintya-Chatterjee@users.noreply.github.com>
2024-12-19 18:29:22 +0530

    chore: remove feature flag from the new progress update flow modal (#1298)

--- src/components/tasks/card/progressContainer/ProgressSlider.tsx
+++ src/components/tasks/card/progressContainer/ProgressSlider.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react';
 
 import { ProgressSliderProps } from '@/interfaces/task.type';
 import styles from '@/components/tasks/card/card.module.scss';
-import { useRouter } from 'next/router';
 
 const ProgressSlider: FC<ProgressSliderProps> = ({
     value,
@@ -10,40 +9,23 @@ const ProgressSlider: FC<ProgressSliderProps> = ({
     handleProgressChange,
     isLoading,
 }) => {
-    const { query } = useRouter();
-    const isDev = query.dev === 'true';
     return (
-        <>
-            {isDev ? (
-                <input
-                    className={styles.slider}
-                    type="range"
-                    value={value}
-                    min="0"
-                    max="100"
-                    step="10"
-                    onChange={(e) => handleProgressChange(e)}
-                    onMouseUp={() => debounceSlider(1000)}
-                    disabled={isLoading}
-                    style={
-                        {
-                            '--progress': `${value}%`,
-                        } as React.CSSProperties
-                    }
-                />
-            ) : (
-                <input
-                    type="range"
-                    value={value}
-                    min="0"
-                    max="100"
-                    step="10"
-                    onChange={(e) => handleProgressChange(e)}
-                    onMouseUp={() => debounceSlider(1000)}
-                    disabled={isLoading}
-                />
-            )}
-        </>
+        <input
+            className={styles.slider}
+            type="range"
+            value={value}
+            min="0"
+            max="100"
+            step="10"
+            onChange={(e) => handleProgressChange(e)}
+            onMouseUp={() => debounceSlider(1000)}
+            disabled={isLoading}
+            style={
+                {
+                    '--progress': `${value}%`,
+                } as React.CSSProperties
+            }
+        />
     );
 };
 
