f17ea1036ca2dfb6f2732bd69a9995f2893c9589
RishiChaubey31 <cenationrishi31@gmail.com>
2025-01-30 21:55:10 +0530

    added one more check to ensure that the onclose() function is working

--- __tests__/Unit/Components/modal/CompletionModal.test.tsx
+++ __tests__/Unit/Components/modal/CompletionModal.test.tsx
@@ -34,14 +34,21 @@ describe('CompletionModal Component', () => {
         expect(queryByText('Congratulations!')).toBeNull();
     });
 
-    test('calls onClose when the Cancel button is clicked', () => {
+    test('calls onClose and removes modal from DOM when Cancel button is clicked', () => {
         const onCloseMock = jest.fn();
-        const { getByRole } = render(
+        const { getByRole, queryByText, rerender } = render(
             <CompletionModal isOpen={true} onClose={onCloseMock} />
         );
 
+        expect(queryByText('Congratulations!')).toBeInTheDocument();
+
         fireEvent.click(getByRole('button', { name: /Cancel/i }));
+
         expect(onCloseMock).toHaveBeenCalledTimes(1);
+
+        rerender(<CompletionModal isOpen={false} onClose={onCloseMock} />);
+
+        expect(queryByText('Congratulations!')).toBeNull();
     });
 
     test('calls onClose when Change Status button is clicked', () => {
